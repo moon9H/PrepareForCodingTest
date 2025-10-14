0487b844f7de5fd34937e670236b01f39b4b973d
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2025-10-14 17:18:04 +0900

    [D3] Title: [파이썬 S/W 문제해결 최적화] 2일차 - 타일 붙이기, Time: 58 ms, Memory: 50,816 KB -BaekjoonHub

--- SWEA/D3/5255. ［파이썬 S／W 문제해결 최적화］ 2일차 － 타일 붙이기/［파이썬 S／W 문제해결 최적화］ 2일차 － 타일 붙이기.py
+++ SWEA/D3/5255. ［파이썬 S／W 문제해결 최적화］ 2일차 － 타일 붙이기/［파이썬 S／W 문제해결 최적화］ 2일차 － 타일 붙이기.py
@@ -1,13 +1,12 @@
-cache = [-1 for _ in range(31)]
-def tiling(width) :
-    global cache
-    if (0 <= width <= 1) : return 1
-    elif width < 0 : return 0
-    if (cache[width] != -1) : return cache[width]
-    cache[width] = tiling(width - 1) + 2*tiling(width - 2) + tiling(width - 3)
-    return cache[width]
-
+t = [1] * 31
+w = 2
+while (w < 31) :
+    if w >= 3 :
+        t[w] = t[w - 1] + 2*t[w - 2] + t[w - 3]
+    else :
+        t[w] = t[w - 1] + 2*t[w - 2]
+    w += 1
 T = int(input())
 for test_case in range(1, T + 1):
     N = int(input())
-    print(f"#{test_case} {tiling(N)}")
+    print(f"#{test_case} {t[N]}")
