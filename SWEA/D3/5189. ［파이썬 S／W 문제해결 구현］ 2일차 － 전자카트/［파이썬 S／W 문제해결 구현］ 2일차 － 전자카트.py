23e95041f736fe7cfe4d435bee26a83264bf52f5
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2025-10-27 16:14:15 +0900

    [D3] Title: [파이썬 S/W 문제해결 구현] 2일차 - 전자카트, Time: 91 ms, Memory: 60,032 KB -BaekjoonHub

--- SWEA/D3/5189. ［파이썬 S／W 문제해결 구현］ 2일차 － 전자카트/［파이썬 S／W 문제해결 구현］ 2일차 － 전자카트.py
+++ SWEA/D3/5189. ［파이썬 S／W 문제해결 구현］ 2일차 － 전자카트/［파이썬 S／W 문제해결 구현］ 2일차 － 전자카트.py
@@ -1,15 +1,46 @@
-from itertools import permutations
 T = int(input())
-for test_case in range(1, T + 1):
+for tc in range(1, T + 1):
     N = int(input())
-    minimum_cost = float('inf')
-    battery_map = [list(map(int, input().split())) for _ in range(N)]
-    area_num = [i for i in range(2, N+1)]
-    perm_list =  list(permutations(area_num))
-    for i in perm_list :
-        possible_path = 0
-        for j in range(len(i) - 1) :
-            possible_path += battery_map[i[j] - 1][i[j+1] - 1]
-        possible_path += battery_map[0][i[0] - 1] + battery_map[i[-1] - 1][0]
-        minimum_cost = possible_path if possible_path < minimum_cost else minimum_cost
-    print(f"#{test_case} {minimum_cost}")
+    W = [list(map(int, input().split())) for _ in range(N)]
+
+    visited = [False] * N
+    visited[0] = True               
+    INF = float('inf')
+    best = INF
+
+    m = INF
+    for i in range(N):
+        for j in range(N):
+            if i != j and W[i][j] < m:
+                m = W[i][j]
+
+    order = [[] for _ in range(N)]
+    for i in range(N):
+        nxts = list(range(1, N))            
+        nxts.sort(key=lambda j: W[i][j])
+        order[i] = nxts
+
+    def dfs(cur, used, cost):
+        global best
+
+        remaining_edges = (N - 1 - used) + 1
+
+        if cost + m * remaining_edges >= best:
+            return
+
+    
+        if used == N - 1:
+            total = cost + W[cur][0]
+            if total < best:
+                best = total
+            return
+
+    
+        for nxt in order[cur]:
+            if not visited[nxt]:
+                visited[nxt] = True
+                dfs(nxt, used + 1, cost + W[cur][nxt])
+                visited[nxt] = False 
+
+    dfs(0, 0, 0)
+    print(f"#{tc} {best}")
