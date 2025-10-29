66c6f1cc528ee0b813a70285cebffa834e74a136
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2025-10-29 15:57:11 +0900

    [D3] Title: [파이썬 S/W 문제해결 구현] 3일차 - 화물 도크, Time: 67 ms, Memory: 52,608 KB -BaekjoonHub

--- SWEA/D3/5202. ［파이썬 S／W 문제해결 구현］ 3일차 － 화물 도크/［파이썬 S／W 문제해결 구현］ 3일차 － 화물 도크.py
+++ SWEA/D3/5202. ［파이썬 S／W 문제해결 구현］ 3일차 － 화물 도크/［파이썬 S／W 문제해결 구현］ 3일차 － 화물 도크.py
@@ -2,14 +2,14 @@ T = int(input())
 for test_case in range(1, T + 1):
     apply_cnt = int(input())
     apply_list = []
-    timeTable = [0 for _ in range(25)]
-    possible_truck = 0
+    possible_truck = 1
     for _ in range(apply_cnt) :
         application = list(map(int, input().split()))
         apply_list.append(application)
     apply_list.sort(key=lambda x:x[1])
-    for i in apply_list :
-        if sum(timeTable[i[0] : i[1]]) == 0 :
+    before_end = apply_list[0][1]
+    for i in range(1, apply_cnt) :
+        if before_end <= apply_list[i][0] :
             possible_truck += 1
-            timeTable[i[0] : i[1]] = [1 for _ in range(i[0], i[1])]
+            before_end = apply_list[i][1]
     print(f"#{test_case} {possible_truck}")
