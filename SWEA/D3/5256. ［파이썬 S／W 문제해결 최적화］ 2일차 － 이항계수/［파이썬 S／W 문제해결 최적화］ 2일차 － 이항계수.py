7f87c6775660b4e24baa66cbe541847ece588816
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2025-11-04 14:51:28 +0900

    [D3] Title: [파이썬 S/W 문제해결 최적화] 2일차 - 이항계수, Time: 58 ms, Memory: 53,632 KB -BaekjoonHub

---
+++ SWEA/D3/5256. ［파이썬 S／W 문제해결 최적화］ 2일차 － 이항계수/［파이썬 S／W 문제해결 최적화］ 2일차 － 이항계수.py
@@ -0,0 +1,18 @@
+def combination(n, a) :
+    if a == 1 or a == n - 1:
+        return n
+    elif a == 0 or a == n:
+        return 1
+    else :
+        mom = 1
+        bro = 1
+        for i in range(n, n - a, -1) :
+            mom *= i
+        for i in range(a, 0, -1) :
+            bro *= i
+        return mom // bro
+    
+T = int(input())
+for test_case in range(1, T + 1):
+    n, a, b = map(int, input().split())
+    print(f"#{test_case} {combination(n, a)}")
