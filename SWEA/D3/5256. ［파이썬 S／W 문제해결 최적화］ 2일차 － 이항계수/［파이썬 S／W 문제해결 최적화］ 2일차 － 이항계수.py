dcea9537eb1d87125ee3158ec27e8603a33400ba
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2025-11-04 15:08:33 +0900

    [D3] Title: [파이썬 S/W 문제해결 최적화] 2일차 - 이항계수, Time: 65 ms, Memory: 58,624 KB -BaekjoonHub

--- SWEA/D3/5256. ［파이썬 S／W 문제해결 최적화］ 2일차 － 이항계수/［파이썬 S／W 문제해결 최적화］ 2일차 － 이항계수.py
+++ SWEA/D3/5256. ［파이썬 S／W 문제해결 최적화］ 2일차 － 이항계수/［파이썬 S／W 문제해결 최적화］ 2일차 － 이항계수.py
@@ -1,19 +1,16 @@
-def combination(n, a) :
-    if a == 1 or a == n - 1:
-        return n
-    elif a == 0 or a == n:
-        return 1
-    else :
-        mom = 1
-        bro = 1
-        for i in range(n, n - a, -1) :
-            mom *= i
-        for i in range(a, 0, -1) :
-            bro *= i
-        return mom // bro
+def comb_dp(n, r) :
+    dp = [[0]*(r + 1) for _ in range(n + 1)]
+    for i in range(n + 1) :
+        for j in range(min(i, r) + 1) :
+            if i == 0 or j == 0 :
+                dp[i][j] = 1
+            else :
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+    
+    return dp[n][r]
     
 T = int(input())
 for test_case in range(1, T + 1):
     n, a, b = map(int, input().split())
     small = a if a < b else b
-    print(f"#{test_case} {combination(n, small)}")
+    print(f"#{test_case} {comb_dp(n, small)}")
