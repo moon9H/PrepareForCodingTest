650ff641c7012135d477e7c0922bec9e700b766f
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2025-11-05 14:36:34 +0900

    [D3] Title: [파이썬 S/W 문제해결 최적화] 1일차 - 접두어 검색, Time: 574 ms, Memory: 64,000 KB -BaekjoonHub

--- SWEA/D3/5253. ［파이썬 S／W 문제해결 최적화］ 1일차 － 접두어 검색/［파이썬 S／W 문제해결 최적화］ 1일차 － 접두어 검색.py
+++ SWEA/D3/5253. ［파이썬 S／W 문제해결 최적화］ 1일차 － 접두어 검색/［파이썬 S／W 문제해결 최적화］ 1일차 － 접두어 검색.py
@@ -3,9 +3,11 @@ for test_case in range(1, T + 1):
     aGroup, bGroup = map(int, input().split())
     a = [input() for _ in range(aGroup)]
     b = [input() for _ in range(bGroup)]
-    isPrefix = [0 for _ in range(bGroup)]
-    for word in a :
-        for j in range(bGroup) :
-            if b[j] in word and not isPrefix[j]:
-                if not word.find(b[j]) : isPrefix[j] += 1
-    print(f"#{test_case} {sum(isPrefix)}")
+    prefix = 0
+    for possible_prefix in b :
+        for word in a :
+            if word.find(possible_prefix) == 0 :
+                prefix += 1
+                break
+    
+    print(f"#{test_case} {prefix}")
