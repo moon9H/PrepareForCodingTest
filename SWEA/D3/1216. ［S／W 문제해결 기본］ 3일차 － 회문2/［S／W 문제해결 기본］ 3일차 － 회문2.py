3e5851e58fe1eeffae13f614a6211226033dfc59
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2025-11-19 15:35:39 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 2,032 ms, Memory: 68,864 KB -BaekjoonHub

--- SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
@@ -1,29 +1,22 @@
 def checkPalindrome(puzzle, palinLen) :
-    palinCnt = 0
     for w in puzzle :
         wordLen = len(w)
         for i in range(wordLen - palinLen + 1) :
             word = w[i : i + palinLen]
             if word == word[::-1] :
-                palinCnt += 1
+                return True
     
-    return palinCnt
+    return False
 
 T = 10
 for test_case in range(1, T + 1):
     tc = int(input())
     wordPuzzle = [input() for _ in range(100)]
     revWordPuzzle = [list(x) for x in zip(*wordPuzzle)]
+    ans = 1
+    for pal_len in range(100, 0, -1):
+        if checkPalindrome(wordPuzzle, pal_len) or checkPalindrome(revWordPuzzle, pal_len):
+            ans = pal_len
+            break
 
-    hor_longestP, vert_longestP = 100, 100
-    hor_p, vert_p = checkPalindrome(wordPuzzle, hor_longestP), checkPalindrome(revWordPuzzle, vert_longestP)
-    while (hor_p == 0) :
-        hor_longestP -= 1
-        hor_p = checkPalindrome(wordPuzzle, hor_longestP)
-    
-    while (vert_p == 0) :
-        vert_longestP -= 1
-        vert_p = checkPalindrome(revWordPuzzle, vert_longestP)
-    
-    longestP = hor_longestP if hor_longestP > vert_longestP else vert_longestP
-    print(f"#{tc} {longestP}")
+    print(f"#{tc} {ans}")
