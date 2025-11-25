1e165f231ac3a63be254830bba22ee5f8b22e6e3
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2025-11-25 22:23:03 +0900

    [Silver II] Title: 최소 힙, Time: 108 ms, Memory: 38340 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -1,42 +1,16 @@
 import sys
 input = sys.stdin.readline
+import heapq
 
 N = int(input())
-heap = [0]
-for _ in range(N) :
-    x = int(input())
-    if x == 0 :
-        if len(heap) <= 1 :
-            print(0)
-        else :
-            print(heap[1])
-            last = heap.pop()
-            if len(heap) == 1 :
-                continue
-            heap[1] = last
-            parent = 1
-            size = len(heap) - 1
-            while True :
-                left = parent * 2
-                right = parent * 2 + 1
-                smallest = parent
+h = []
 
-                if left <= size and heap[left] < heap[smallest] :
-                    smallest = left
-                if right <= size and heap[right] < heap[smallest] :
-                    smallest = right
-                
-                if smallest == parent :
-                    break
-                
-                heap[parent], heap[smallest] = heap[smallest], heap[parent]
-                parent = smallest
-    else :
-        heap.append(x)
-        size = len(heap)
-        child = size - 1
-        parent = child // 2
-        while (heap[parent] > heap[child]) :
-            heap[parent], heap[child] = heap[child], heap[parent]
-            child = parent
-            parent //= 2
+for _ in range(N):
+    x = int(input())
+    if x == 0:
+        if h:
+            print(heapq.heappop(h))
+        else:
+            print(0)
+    else:
+        heapq.heappush(h, x)
