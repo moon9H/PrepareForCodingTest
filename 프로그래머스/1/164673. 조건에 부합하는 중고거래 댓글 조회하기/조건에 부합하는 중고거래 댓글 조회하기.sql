e11d8552dc832f8b9ba61115aabc0093ec2d6e57
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-01-01 19:24:56 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,13 @@
+-- 코드를 입력하세요
+SELECT 
+    TITLE, 
+    ugb.BOARD_ID, 
+    REPLY_ID, 
+    ugr.WRITER_ID, 
+    ugr.CONTENTS, 
+    DATE_FORMAT(ugr.CREATED_DATE, '%Y-%m-%d') AS CREATED_DATE 
+    FROM USED_GOODS_BOARD as ugb 
+    INNER JOIN USED_GOODS_REPLY as ugr 
+    ON ugb.BOARD_ID = ugr.BOARD_ID
+    WHERE DATE_FORMAT(ugb.CREATED_DATE, '%Y-%m') = '2022-10'
+    ORDER BY ugr.CREATED_DATE ASC, TITLE ASC;
