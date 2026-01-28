011b560c4a66bd7f8b89a07cdd2f0a5fe52ce8f2
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-01-28 23:48:46 +0900

    Title: 달에서 무게 구하기, Time: 92ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260128/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int weight = 13;
+        double gravity = 0.165;
+
+        System.out.println(weight + " * " + gravity + " = " + (weight * gravity));
+    }
+}
