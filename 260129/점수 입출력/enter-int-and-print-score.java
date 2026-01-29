11ccc44e1da48f262cb81ab059395e393fc10472
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-01-29 09:00:59 +0900

    Title: 점수 입출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260129/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+        System.out.printf("Your score is %d point.\n", sco);
+    }
+}
