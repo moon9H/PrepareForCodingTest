937503b66d904361007770b7a9229ad3892564cd
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-02-02 15:34:09 +0900

    [Silver III] Title: 꿀 아르바이트, Time: 196 ms, Memory: 25588 KB -BaekjoonHub

--- 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.java
+++ 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.java
@@ -6,24 +6,25 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine());
+		
 		int[] dailyPay = new int[N];
 		for (int i = 0; i < N; i++) {
 			dailyPay[i] = Integer.parseInt(st.nextToken());
 		}
-		long sum = -1;
 		long rangeSum = 0;
-		for (int i = 0; i < N - M + 1; i++) {
-			for (int j = 0; j < M; j++) {
-				rangeSum += dailyPay[i + j];
-			}
-			sum = (rangeSum > sum) ? rangeSum : sum;
-			rangeSum = 0;
+		for (int i = 0; i < M; i++) {
+			rangeSum += dailyPay[i];
 		}
-		System.out.println(sum);
+		long maxSum = rangeSum;
+		for (int i = M; i < N; i++) {
+			rangeSum = rangeSum - (dailyPay[i - M]) + dailyPay[i];
+			if (rangeSum > maxSum) maxSum = rangeSum;
+		}
+		System.out.println(maxSum);
 	}
 }
