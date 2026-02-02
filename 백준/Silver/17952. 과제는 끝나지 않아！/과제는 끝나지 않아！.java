adb7555fda25d085796714a82deef65e7790ac5e
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-02-02 10:46:08 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 992 ms, Memory: 232272 KB -BaekjoonHub

---
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		Stack<Integer[]> stack = new Stack<>();
+		int score = 0;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			if (Integer.parseInt(st.nextToken()) == 1) {
+				int point = Integer.parseInt(st.nextToken());
+				int time = Integer.parseInt(st.nextToken());
+				if (time - 1 == 0) {
+					score += point;
+				} else {
+					stack.add(new Integer[] {point, time - 1});
+				}
+			} else {
+				if (!stack.isEmpty()) {
+					Integer[] top = stack.peek();
+					top[1] -= 1;
+					if (top[1] == 0) {
+						score += top[0];
+						stack.pop();
+					} else {
+						stack.set(stack.size() - 1, top);
+					}
+				}
+			}
+		}
+		System.out.println(score);
+	}
+}
