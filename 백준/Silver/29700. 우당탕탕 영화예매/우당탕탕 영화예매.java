22d9621158e12d350d09bbda8635d753858a9544
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-02-03 15:59:55 +0900

    [Silver IV] Title: 우당탕탕 영화예매, Time: 256 ms, Memory: 54436 KB -BaekjoonHub

--- 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.java
+++ 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.java
@@ -4,42 +4,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken()); // 행 개수 
-		int M = Integer.parseInt(st.nextToken()); // 열 개수
-		int K = Integer.parseInt(st.nextToken()); // 동아리원 수 
-		int[][] arr = new int[N][M]; // 좌석 현황 배열 
-		int result = 0; // 가능한 좌석 개수 
+	public static void main(String[] args) throws IOException{
+		// 입력 처리
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[][] seats = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			String line = br.readLine();
+			for (int j = 0; j < M; j++) {
+				seats[i][j] = line.charAt(j) - '0';
+			}
+		}
 		
-		// 입력 값 배열에 담기 
+		// 메인 로직 시작
+		int result = 0;
 		for (int i = 0; i < N; i++) {
-			String line = in.readLine();
-			for (int j = 0; j < M; j++) {
-				arr[i][j] = line.charAt(j) - '0'; // '0' 빼서 숫자로 담기!!! 
+			if (K > M) break;
+			int sum = 0;
+			for (int j = 0; j < K; j++) {
+				sum += seats[i][j];
+			}
+			if (sum == 0) result ++;
+			for (int j = 1; j < M - K + 1; j++) {
+				sum -= seats[i][j - 1];
+				sum += seats[i][j + K - 1];
+				if (sum == 0) result++;
 			}
 		}
-
-		for (int i = 0; i < N; i++) {
-			int count = 0; // 비어있는 좌석의 개수 
-			for (int j = 0; j < M; j++) {
-				if (arr[i][j] == 0) {
-					count++; // 0인 요소를 만나면 증가
-				} else {
-					// 1인 요소를 만났을 때 count에 누적된 값이 K보다 같거나 크면 result에 반영 
-					if (count >= K) {
-						result += (count - K + 1); 
-					}
-					// count 초기화 
-					count = 0;
-				}
-			}
-			// 마지막이 0으로 끝났을 때를 위한 처리 
-			if (count >= K) {
-				result += (count - K + 1);
-			}
-		}
-		System.out.print(result);
+		System.out.println(result);
 	}
 }
