38226c4bb6d1edab7e3c8fdd970df34418b57003
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-02-03 17:42:45 +0900

    [Silver IV] Title: 주몽, Time: 176 ms, Memory: 16552 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -5,32 +5,43 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int M = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] numList = new int[N];
-		
-		for (int i = 0; i < N; i++) {
-			numList[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(numList);
-		int s = 0, l = N - 1;
-		int pairCnt = 0;
-		int sum = 0;
-		while (l > s) {
-			sum = numList[s] + numList[l];
-			if (sum > M) {
-				l--;
-			} else if (sum < M) {
-				s++;
-			} else {
-				s++;
-				l--;
-				pairCnt++;
-			}
-		}
-		System.out.println(pairCnt);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine()); // 재료 개수 
+        int target = Integer.parseInt(in.readLine()); // 필요한 재료 수 
+
+        // 배열에 재료 넣기 
+        StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        // 배열 정렬 
+        Arrays.sort(arr);
+        
+        int left = 0; 
+        int right = N - 1; 
+        int sum = 0;
+        int cnt = 0; 
+        
+        // left < right 가 유지될 때까지 순회 
+        while (left < right) {
+        	// 두 재료만 사용해야 하므로 두 개 합산 
+        	sum = arr[left] + arr[right]; 
+        	// target과 합이 동일하면 cnt 올리고 범위 좁히기 
+        	if (sum == target) {
+        		cnt++;
+        		left++;
+        		right--;
+        	} else if (sum < target) {
+        		// 합산 값이 target 보다 작으면 최솟값 올리기 
+        		left++;
+        	} else {
+        		// sum이 target 보다 커지면 최댓값 내리기 
+        		right--;
+        	}
+        }
+        System.out.println(cnt);
+    }
 }
