2b1b43f574d2c7e712e0976a6eda7cf650f004b6
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-02-04 21:16:02 +0900

    [Silver IV] Title: 상근이의 여행, Time: 240 ms, Memory: 44552 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,17 +12,11 @@ public class Main {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
-            int airplaneNum = 0;
-            ArrayList<Integer>[] adj = new ArrayList[N + 1];
-            for (int j = 1; j <= N; j++) adj[j] = new ArrayList<Integer>();
             for (int j = 0; j < M; j++){
                 st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                adj[a].add(b);
-                adj[b].add(a);
+                st.nextToken();
+                st.nextToken();
             }
-
             sb.append(N - 1).append("\n");
         }
         System.out.println(sb.toString());
