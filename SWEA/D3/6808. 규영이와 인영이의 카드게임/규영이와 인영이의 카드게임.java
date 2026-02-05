7739854a646825b59549a9a51d68cbc66b8c8748
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-02-05 14:48:53 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 1,451 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -7,7 +7,6 @@ public class Solution {
 	static int winCnt, loseCnt;
 	static int[] inyoungCardsList;
 	static int[] gyuyoungCards;
-	static int[] numbers;
 	private static boolean[] isSelected;
 	
 	public static void main(String[] args) throws IOException{
@@ -16,15 +15,16 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 		for (int testCase = 1; testCase <= T; testCase++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			winCnt = 0;
-			loseCnt = 0;
+			winCnt = loseCnt = 0;
+			
 			gyuyoungCards = new int[9];
+			inyoungCardsList = new int[9];
+			
 			isSelected = new boolean[9];
-			numbers = new int[9];
+			
 			for (int i = 0; i < 9; i++) {
 				gyuyoungCards[i] = Integer.parseInt(st.nextToken());
 			}
-			inyoungCardsList = new int[9];
 			int idx = 0;
 			for (int i = 1; i <= 18; i++) {
 				boolean inGyu = false;
@@ -37,16 +37,18 @@ public class Solution {
 				if (!inGyu)
 					inyoungCardsList[idx++] = i;
 			}
-			permutation(0);
+			
+			permutation(0, 0, 0);
 			sb.append("#").append(testCase).append(" ").
 			append(winCnt).append(" ").append(loseCnt).append("\n");
 		}
 		System.out.println(sb);
 	}
 	
-	public static void permutation(int cnt) {
+	public static void permutation(int cnt, int gyuScore, int inScore) {
 		if (cnt == 9) {
-			playGame(gyuyoungCards, numbers);
+			if (gyuScore > inScore) winCnt++;
+			else loseCnt++;
 			return;
 		}
 		
@@ -54,22 +56,13 @@ public class Solution {
 			if (isSelected[i]) continue;
 			
 			isSelected[i] = true;
-			numbers[cnt] = inyoungCardsList[i];
-			permutation(cnt + 1);
+			int roundScore =  inyoungCardsList[i] + gyuyoungCards[cnt];
+			if (gyuyoungCards[cnt] > inyoungCardsList[i])
+				permutation(cnt + 1, gyuScore + roundScore, inScore);
+			else
+				permutation(cnt + 1, gyuScore, inScore + roundScore);
+			
 			isSelected[i] = false;
 		}
-	}
-	
-	public static void playGame(int[] gyuyoung, int[] inyoung) {
-		int inyoungScore = 0, gyuyoungScore = 0;
-		for (int i = 0; i < 9; i++) {
-			if (gyuyoung[i] > inyoung[i]) {
-				gyuyoungScore += gyuyoung[i] + inyoung[i];
-			} else {
-				inyoungScore += gyuyoung[i] + inyoung[i];
-			}
-		}
-		if (gyuyoungScore > inyoungScore) winCnt++;
-		else loseCnt++;
 	}
 }
