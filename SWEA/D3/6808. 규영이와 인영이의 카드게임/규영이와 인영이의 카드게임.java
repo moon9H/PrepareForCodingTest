f26ea073da5b8e1b545ada9fafb828301100802e
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-02-05 15:21:49 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 2,271 ms, Memory: 26,752 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -1,67 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
+	static int[] gArr, iArr;
+	static int M = 9;
 	static int winCnt, loseCnt;
-	static int[] inyoungCardsList;
-	static int[] gyuyoungCards;
-	private static boolean[] isSelected;
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		for (int testCase = 1; testCase <= T; testCase++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int tc = Integer.parseInt(in.readLine());
+
+		for (int test_case = 1; test_case <= tc; test_case++) {
+			boolean[] picked = new boolean[2 * M + 1];
+			gArr = new int[M];
+			iArr = new int[M];
+
 			winCnt = loseCnt = 0;
-			
-			gyuyoungCards = new int[9];
-			inyoungCardsList = new int[9];
-			
-			isSelected = new boolean[9];
-			
-			for (int i = 0; i < 9; i++) {
-				gyuyoungCards[i] = Integer.parseInt(st.nextToken());
+			StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+			for (int i = 0; i < M; i++) {
+				gArr[i] = Integer.parseInt(st.nextToken());
+				picked[gArr[i]] = true;
 			}
-			int idx = 0;
-			for (int i = 1; i <= 18; i++) {
-				boolean inGyu = false;
-				for (int j = 0; j < 9; j++) {
-					if (gyuyoungCards[j] == i) {
-						inGyu = true;
-						break;
-					}
-				}
-				if (!inGyu)
-					inyoungCardsList[idx++] = i;
+
+			for (int i = 1, j = 0; i < 2 * M + 1; i++) {
+				if (!picked[i])
+					iArr[j++] = i;
 			}
 			
-			permutation(0, 0, 0);
-			sb.append("#").append(testCase).append(" ").
-			append(winCnt).append(" ").append(loseCnt).append("\n");
+			permutation(0, new boolean[M], 0, 0);			
+			System.out.println("#" + test_case + " " + winCnt + " " + loseCnt);
 		}
-		System.out.println(sb);
 	}
 	
-	public static void permutation(int cnt, int gyuScore, int inScore) {
-		if (cnt == 9) {
-			if (gyuScore > inScore) winCnt++;
-			else loseCnt++;
+	// 인영이의 카드 순서를 결정하는 메서드 
+	public static void permutation(int cnt, boolean[] isSelected, int gScore, int iScore) {
+		
+		if(cnt == M) {
+			if(gScore> iScore) ++winCnt;
+			else ++loseCnt;
 			return;
 		}
 		
-		for (int i = 0; i < 9; i++) {			
+		for (int i = 0; i < M; i++) {
 			if (isSelected[i]) continue;
-			
 			isSelected[i] = true;
-			int roundScore =  inyoungCardsList[i] + gyuyoungCards[cnt];
-			if (gyuyoungCards[cnt] > inyoungCardsList[i])
-				permutation(cnt + 1, gyuScore + roundScore, inScore);
-			else
-				permutation(cnt + 1, gyuScore, inScore + roundScore);
-			
+			int sum = gArr[cnt] + iArr[i];
+			int diff = gArr[cnt] - iArr[i];
+			permutation(cnt + 1, isSelected, gScore+(diff>0?sum:0), iScore+(diff<0?sum:0));
 			isSelected[i] = false;
 		}
 	}
