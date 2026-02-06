4490d20f3f61e396475dc7a8a41eccc064b1197e
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-02-06 15:31:24 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 105 ms, Memory: 27,392 KB -BaekjoonHub

--- SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
@@ -4,82 +4,81 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	private static int C, M, N,  maxSumA, maxSumB;
 	
+	private static int N, M, C;
+	private static int[][] map, maxMap;			// 입력받은 벌통의 상태 Map, 각 위치에서 연속된 M개의 별통 중 최대 이익
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= T; tc++) {
-			// 입력 처리
+		int TC = Integer.parseInt(br.readLine());
+		
+		for (int tc = 1; tc <= TC; tc++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			M = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken()); 
+			M  = Integer.parseInt(st.nextToken()); 
 			C = Integer.parseInt(st.nextToken());
-			maxSumA = maxSumB = 0;
-			int ans = 0;
-			int[][] hive = new int[N][N];
+			
+			map = new int[N][N];
+			maxMap = new int[N][N - M + 1];
+			
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
-					hive[i][j] = Integer.parseInt(st.nextToken());
+					map[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-			
-			for (int i = 0; i < N; i++) {
-				int[] workerA = new int[M];
-				for (int j = 0; j < N - M + 1; j++) {
-					// 일꾼 A 배열 준비
-					for (int idx = 0; idx < M; idx++) {
-						workerA[idx] = hive[i][j + idx];
-					}
-					maxSumA = 0;
-					combination(workerA, 0, 0, 0, 'A');
-					for (int k = i; k < N; k++) {
-						int[] workerB = new int[M];
-						for (int h = (k == i) ? j + M : 0; h < N - M + 1; h++) {
-							if (h + M - 1 >= N) continue;
-							
-							for (int idx = 0; idx < M; idx++) {
-								workerB[idx] = hive[k][h + idx];
-							}
-							
-							maxSumB = 0;
-							combination(workerB, 0, 0, 0, 'B');
-							
-							ans = Math.max(ans, maxSumA + maxSumB);
-						}
-						
-					}
-				}
+			System.out.println("#" + tc + " " + getMaxBenefit());
+		}
+	}
+	
+	private static int getMaxBenefit() {
+		// 연속된 M개를 선택할 수 있는 모든 행의 열의 위치마다 최대 이익 계산
+		// 두 일꾼의 선택 조합
+		makeMaxMap();
+		
+		return processCombination();
+	}
+	
+	private static void makeMaxMap() {
+		for (int i = 0; i < N; i++) {			// 각 행마다 반복
+			for (int j = 0; j < N - M + 1; j++) {
+				// i, j 위치에서 부분집합을 따져보고 최대이익 계산
+				subset(i, j, 0, 0, 0);
 			}
-			sb.append("#").append(tc).append(" ").append(ans).append("\n");
+		}
+	}
+	
+	private static void subset(int i, int j, int cnt, int sum, int powSum) {
+		if (sum > C) return;
+		
+		if (cnt == M) {
+			maxMap[i][j - M] = Math.max(maxMap[i][j - M], powSum);			
+			return ;
 		}
 		
-		System.out.println(sb);
+		// i, j 벌통을 포함
+		subset(i, j + 1, cnt + 1, sum + map[i][j], powSum + map[i][j] * map[i][j]);
+		// i, j 벌통을 미포함
+		subset(i, j + 1, cnt + 1, sum, powSum);
 	}
-
 	
-	public static void combination(int[] honeyLine, int cnt, int honeySum, int profitSum, char human){
-        // 칼로리 초과면 볼 필요 없음
-        if (honeySum > C) return;
-        
-        if (human == 'A') {
-        	maxSumA = Math.max(maxSumA, profitSum);
-        } else {
-        	maxSumB = Math.max(maxSumB, profitSum);
-        }
-        
-        // 끝까지 갔다면 종료
-        if (cnt == M) return;
-
-        // cnt번째 재료 선택하는 경우
-        combination(honeyLine, cnt + 1,
-                honeySum + honeyLine[cnt],
-                profitSum + honeyLine[cnt] * honeyLine[cnt], human);
-
-        // cnt번째 재료 선택하지 않는 경우
-        combination(honeyLine, cnt + 1, honeySum, profitSum, human);
-    }
-	
+	private static int processCombination() {
+		
+		int aBenefit, bBenefit, max = 0;
+		// 일꾼 A 선택
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N - M + 1; j++) {
+				aBenefit = maxMap[i][j];
+				// 일꾼 B 선택
+				bBenefit = 0;
+				for (int i2 = i; i2 < N; i2++) {
+					int start = (i == i2)? j + M : 0;
+					for (int j2 = start; j2 < N - M + 1; j2++) {
+						bBenefit = Math.max(bBenefit, maxMap[i2][j2]);
+					}
+				}
+				max = Math.max(max, aBenefit + bBenefit);
+			}
+		}
+		return max;
+	}
 }
