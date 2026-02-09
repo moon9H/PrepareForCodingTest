2ab5682ef3b421a73077d1c839dc6312fd99b245
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-02-09 16:26:14 +0900

    [Silver III] Title: 다음 순열, Time: 228 ms, Memory: 16484 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static int N;
+	private static int[] input;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		input = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		if (np())
+			for (int a : input) System.out.print(a + " ");
+		else
+			System.out.println(-1);
+	}
+	
+	private static boolean np() {
+		int i = N - 1;
+		
+		while (i > 0 && input[i - 1] >= input[i]) --i;
+		
+		if (i == 0) return false;
+		
+		int j = N - 1;
+		while (input[i - 1] >= input[j]) --j;
+		
+		swap(i - 1, j);
+		 
+		int k = N - 1;
+		while (i < k) swap(i++, k--);
+		
+		return true;
+	}
+	
+	private static void swap(int a, int b) {
+		int temp = input[a];
+		input[a] = input[b];
+		input[b] = temp;
+	}
+}
