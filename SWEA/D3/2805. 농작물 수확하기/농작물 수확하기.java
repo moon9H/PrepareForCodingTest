ffae38e6f28278165addbd0b931200464cef53d9
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-02-11 14:55:54 +0900

    [D3] Title: 농작물 수확하기, Time: 88 ms, Memory: 25,344 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -10,24 +10,18 @@ public class Solution {
 		for (int tc = 1; tc <= T; tc++) {
 			int N = Integer.parseInt(br.readLine());
 			int[][] farm = new int[N][N];
-			
-			for (int i = 0; i < N; i++) {
-				String line = br.readLine();
-				for (int j = 0; j < N; j++)
-					farm[i][j] = line.charAt(j) - '0';
-			}
-			
 			int cRow = N / 2;
 			int cCol = N / 2;
 			int sum = 0;
-			for (int row = cRow - N / 2; row <= cRow + N / 2; row++) {
-				for (int col = cCol - N / 2; col <= cCol + N / 2; col++) {
-					if (Math.abs(cRow - row) + Math.abs(cCol - col) <= N/2) {
-						sum += farm[row][col];
+			for (int i = 0; i < N; i++) {
+				String line = br.readLine();
+				for (int j = 0; j < N; j++) {
+					farm[i][j] = line.charAt(j) - '0';
+					if (Math.abs(cRow - i) + Math.abs(cCol - j) <= N/2) {
+						sum += farm[i][j];
 					}
 				}
 			}
-			
 			sb.append("#" + tc + " ").append(sum).append("\n");
 		}
 		System.out.println(sb);
