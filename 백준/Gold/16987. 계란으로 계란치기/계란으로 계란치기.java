3a02137e7024874b29468efdf60a2eee67b0fd4a
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-02-11 10:01:34 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 244 ms, Memory: 18844 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -4,54 +4,82 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static int N, maxBreakEggs;
-	private static int[][] eggs;
+	private static int N, total = 0;
+	private static int[] strength, weight;
+	private static boolean[] isBroken;
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
+		// 왼쪽 계란부터 선택하며 다른 계란 중에 하나와 부딪혀서 내구도 감소시킴
+		// 치고 나서 선택은 바로 오른쪽 계란
+		// 가장 오른쪽 계란을 선택하고 위 과정을 거친 뒤 프로그램 종료
+		// => 최적의 선택이 되려면 ? => 일단 재귀로 완탐 해보자..
+		// 입력: 계란 개수 \n 내구도 무게
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		maxBreakEggs = 0;
-		eggs = new int[N][2];
+		strength = new int[N];
+		weight = new int[N];
+
 		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			eggs[i][0] = Integer.parseInt(st.nextToken());							// 내구도
-			eggs[i][1] = Integer.parseInt(st.nextToken());							// 무게
-		
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			strength[i] = Integer.parseInt(st.nextToken());
+			weight[i] = Integer.parseInt(st.nextToken());
 		}
-		breakEggs(0, 0);
-		
-		System.out.println(maxBreakEggs);
+//		System.out.println(strength[0]+ " " + weight[0]);
+
+		// 계란의 파손 여부 저장
+		isBroken = new boolean[N];
+
+		// 재귀 함수
+		dfs(0, isBroken);
+
+		// 출력
+		System.out.println(total);
+	}
+
+	public static void dfs(int idx, boolean[] isBroken) {
+		if (idx == N) {
+			int count = 0;
+
+			for (int i = 0; i < N; i++) {
+				if (strength[i] <= 0)
+					count++;
+			}
+
+			total = Math.max(count, total);
+			return;
+		}
+
+		// 깨진 계란 개수가 n-1이면 종료
+//		int n = 0;
+//		for (int j = 0; j < N; j++) {
+//			if (isBroken[j])
+//				n++;
+//		}
+
+		if (strength[idx] <= 0 || allOthersBroken(idx)) {
+			dfs(idx + 1, isBroken);
+			return;
+		}
+
+		// 현재 계란에서 몇 번째 계란을 선택할지에 따라 경우가 나뉨
+		for (int i = 0; i < N; i++) {
+			if (i == idx || strength[i] <= 0)
+				continue;
+
+			strength[idx] -= weight[i];
+			strength[i] -= weight[idx];
+
+			dfs(idx + 1, isBroken);
+
+			strength[idx] += weight[i];
+			strength[i] += weight[idx];
+		}
 	}
 	
-	
-	static void breakEggs(int index, int sum) {
-		if (2 * (N - index + sum) < maxBreakEggs) return;
-		
-		if (index == N) {
-			maxBreakEggs = Math.max(maxBreakEggs, sum);
-			return;
-		}
-		
-		if (eggs[index][0] <= 0 || sum == N - 1) {
-			breakEggs(index + 1, sum);
-			return;
-		}
-		
-		for (int i = 0; i < N; i++) {
-			if (i == index || eggs[i][0] <= 0) continue;
-			
-			
-			eggs[index][0] -= eggs[i][1];
-			eggs[i][0] -= eggs[index][1];
-			
-			int broken = 0;
-			if (eggs[index][0] <= 0) ++broken;
-			if (eggs[i][0] <= 0) ++broken;
-			
-			breakEggs(index + 1, sum + broken);
-			
-			eggs[index][0] += eggs[i][1];
-			eggs[i][0] += eggs[index][1];
-		}	
+	private static boolean allOthersBroken(int currentIdx) {
+	    for (int i = 0; i < N; i++) {
+	        if (i != currentIdx && strength[i] > 0) return false;
+	    }
+	    return true;
 	}
 }
