1a5e04acbe66e24407ae65b847d9c8f3a1b1234c
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-02-12 16:44:45 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1428 ms, Memory: 124880 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,34 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		// 입력 처리
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
-		// 상근이가 가지고 있는 숫자 카드 입력
-		int[] cards = new int[20_000_001];
-		Set<Integer> set = new HashSet<>();
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			int n = Integer.parseInt(st.nextToken()) + 10_000_000;
-			++cards[n];
-			set.add(n);
-		}
-		
-		
-		int M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < M; i++) {
-			int m = Integer.parseInt(st.nextToken()) + 10_000_000;
-			sb.append(set.contains(m) ? cards[m] + " " : 0 + " ");
-		}
-		System.out.println(sb);
-	}
+    static int[] base;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        base = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) base[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(base);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+
+            int count = upperBound(target) - lowerBound(target);
+            sb.append(count).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+
+    static int lowerBound(int target) {
+        int low = 0, high = base.length;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (base[mid] >= target) high = mid;
+            else low = mid + 1;
+        }
+        return low;
+    }
+
+    static int upperBound(int target) {
+        int low = 0, high = base.length;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (base[mid] > target) high = mid;
+            else low = mid + 1;
+        }
+        return low;
+    }
 }
