977e9b09eb96ca9c31b25f3442de0d54966f7202
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-02-16 22:35:17 +0900

    [D2] Title: 나무 높이, Time: 92 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -1,59 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
-
-        for (int tc = 1; tc <= T; tc++) {
+        for (int tc = 1; tc <= T ; tc++) {
             int N = Integer.parseInt(br.readLine());
             int[] trees = new int[N];
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int tallest = 0;
             for (int i = 0; i < N; i++) {
                 trees[i] = Integer.parseInt(st.nextToken());
-                tallest = Math.max(tallest, trees[i]);
             }
+
+            Arrays.sort(trees);
 
             int minDays = Integer.MAX_VALUE;
 
-            for (int target = tallest; target <= tallest + 1; target++) {
-                int oddTrees = 0;  // 1만큼 더 자라야 하는 횟수 (홀수날용)
-                int evenTrees = 0; // 2만큼 더 자라야 하는 횟수 (짝수날용)
+            for (int target = trees[N - 1]; target <= trees[N - 1] + 1; target++) {
+                int even = 0, odd = 0;
+                int ans = 0;
 
-                for (int i = 0; i < N; i++) {
-                    int diff = target - trees[i];
-                    if (diff == 0) continue;
-
-                    evenTrees += diff / 2;
-                    oddTrees += diff % 2;
-                }
-
-                // 2를 1+1로 쪼개서 균형 맞추기 (짝수날이 너무 많이 남는 경우 방지)
-                if (evenTrees > oddTrees) {
-                    while (Math.abs(evenTrees - oddTrees) > 1) {
-                        evenTrees--;
-                        oddTrees += 2;
+                for (int i = 0; i < N - 1; i++) {
+                    int residual = target - trees[i];
+                    if (residual > 0){
+                        even += residual / 2;
+                        odd += residual % 2;
                     }
                 }
 
-                int ans = 0;
-                if (oddTrees > evenTrees) {
-                    // 홀수날이 더 많으면 홀-짝-홀-짝-홀... 순서로 끝남
-                    ans = oddTrees * 2 - 1;
-                } else if (evenTrees > oddTrees) {
-                    // 짝수날이 더 많으면 (위의 조절로 최대 1개 차이) 짝수날에 끝남
-                    ans = evenTrees * 2;
-                } else {
-                    // 개수가 같으면 모든 날을 다 씀
-                    ans = oddTrees + evenTrees;
+                if (even > odd){
+                    while (Math.abs(even - odd) > 1){
+                        even--;
+                        odd += 2;
+                    }
                 }
-                
+
+                if (odd > even){
+                    ans = 2 * odd - 1;
+                } else if (even > odd){
+                    ans = 2 * even;
+                } else {
+                    ans = even + odd;
+                }
+
                 minDays = Math.min(minDays, ans);
             }
 
