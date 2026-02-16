b08a63fa00d98abd2b25cc21ded2631eae79675d
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-02-16 19:32:45 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 171 ms, Memory: 30,276 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -2,97 +2,88 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
- 
+
 public class Solution {
-    private static int N, maxCore, minSum;
+    private static int N, needToConnectCores, maxCores, minLineSum;
     private static int[][] board;
-    private static List<int[]> corePos;
-    private final static int[] dirRow = {-1, 0, 1, 0};
-    private final static int[] dirCol = {0, 1, 0, -1};
- 
+    private static final int[] dr = {-1, 1, 0, 0};
+    private static final int[] dc = {0, 0, -1, 1};
+    private static ArrayList<int[]> corePos;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
- 
-        for (int t = 1; t <= T; t++) {
+        for (int tc = 1; tc <= T ; tc++) {
             N = Integer.parseInt(br.readLine());
-            board = new int[N][N];
+            board = new int[N][N];needToConnectCores = maxCores = 0;
+            minLineSum = Integer.MAX_VALUE;
             corePos = new ArrayList<>();
-            maxCore = 0;
-            minSum = Integer.MAX_VALUE;
- 
             for (int i = 0; i < N; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++) {
                     board[i][j] = Integer.parseInt(st.nextToken());
                     if (board[i][j] == 1) {
-                        if (!(i == 0 || i == N - 1 || j == 0 || j == N - 1)) {
+                        if (i > 0 && i < N - 1 && j > 0 && j < N - 1) {
+                            needToConnectCores++;
                             corePos.add(new int[]{i, j});
                         }
                     }
                 }
             }
-            setLine(0, 0, 0);
-             
-            sb.append("#").append(t).append(" ").append(minSum).append("\n");
+
+            dfs(0, 0, 0);
+
+            sb.append('#').append(tc).append(' ').append(minLineSum).append('\n');
         }
         System.out.println(sb);
     }
- 
-    static void setLine(int coreIdx, int connectedCore, int lineSum) {
-    	if (connectedCore + corePos.size() - coreIdx < maxCore) return;
-        
-    	if (coreIdx == corePos.size()) {
-            if (connectedCore > maxCore) {
-                maxCore = connectedCore;
-                minSum = lineSum;
-            } else if (connectedCore == maxCore) {
-                minSum = Math.min(minSum, lineSum);
-            }
+
+    static void dfs(int count, int lineSum, int coreSum){
+        if (count == needToConnectCores){
+            if (coreSum > maxCores){
+                maxCores = coreSum;
+                minLineSum = lineSum;
+            } else if (coreSum == maxCores) minLineSum = Math.min(minLineSum, lineSum);
+            
             return;
         }
- 
-        int[] cur = corePos.get(coreIdx);
-        int r = cur[0];
-        int c = cur[1];
- 
+
+        int[] curCore = corePos.get(count);
+
         for (int i = 0; i < 4; i++) {
-            if (linePossible(r, c, i)) {
-                int dist = connect(r, c, i, 1);							// 해당 방향으로 전선 연결 가능
-                setLine(coreIdx + 1, connectedCore + 1, lineSum + dist);
-                connect(r, c, i, 0); 									// 전선 연결 해제
+            if (checkLine(curCore[0], curCore[1], i)){
+                int lineLength = makeLine(curCore[0], curCore[1], i, 1);
+                dfs(count + 1, lineSum + lineLength, coreSum + 1);
+                makeLine(curCore[0], curCore[1], i, 0);
             }
         }
- 
-        setLine(coreIdx + 1, connectedCore, lineSum);						// 전선을 연결할 수 없는 경우
+
+        dfs(count + 1, lineSum, coreSum);
     }
- 
-    static boolean linePossible(int curRow, int curCol, int dir) {
-        int nr = curRow + dirRow[dir];
-        int nc = curCol + dirCol[dir];
- 
-        while (nr >= 0 && nr < N && nc >= 0 && nc < N) {
+
+    static boolean checkLine(int row, int col, int dir){
+        int nr = row + dr[dir];
+        int nc = col + dc[dir];
+
+        while(nr >= 0 && nr < N && nc >= 0 && nc < N){
             if (board[nr][nc] == 1) return false;
-            nr += dirRow[dir];
-            nc += dirCol[dir];
+            nr += dr[dir];
+            nc += dc[dir];
         }
+
         return true;
     }
- 
-    static int connect(int curRow, int curCol, int dir, int state) {
-        int nr = curRow + dirRow[dir];
-        int nc = curCol + dirCol[dir];
-        int lineLength = 0;
- 
-        while (nr >= 0 && nr < N && nc >= 0 && nc < N) {
-            board[nr][nc] = state;
-            lineLength++;
-            nr += dirRow[dir];
-            nc += dirCol[dir];
+    static int makeLine(int row, int col, int dir, int mode){
+        int nr = row + dr[dir];
+        int nc = col + dc[dir];
+        int length = 0;
+        while(nr >= 0 && nr < N && nc >= 0 && nc < N){
+            board[nr][nc] = mode;
+            ++length;
+            nr += dr[dir];
+            nc += dc[dir];
         }
-        return lineLength;
+        return length;
     }
 }
