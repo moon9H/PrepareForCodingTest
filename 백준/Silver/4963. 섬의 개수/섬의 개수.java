d688ba96d7e96bd7d3e3fe3953ef0458c08a3868
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-02-21 12:00:00 +0900

    [Silver II] Title: 섬의 개수, Time: 124 ms, Memory: 14696 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,76 +1,58 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
 
-/* 섬의 개수 */
 public class Main {
-    static int w, h;
-    static int[][] map;
-    static boolean[][] visited;
-
-    static int[] dx = {-1,-1,-1,0,0,1,1,1};
-    static int[] dy = {-1,0,1,-1,1,-1,0,1};
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        while (true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            w = Integer.parseInt(st.nextToken());
-            h = Integer.parseInt(st.nextToken());
-
-            if (w == 0 && h == 0) break;
-
-            map = new int[h][w];
-            visited = new boolean[h][w];
-
-            for (int i = 0; i < h; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < w; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-
-            int count = 0;
-
-            // 전체 탐색
-            for (int i = 0; i < h; i++) {
-                for (int j = 0; j < w; j++) {
-                    if (map[i][j] == 1 && !visited[i][j]) {
-                        bfs(i, j);
-                        count++;
-                    }
-                }
-            }
-
-            System.out.println(count);
-        }
-    }
-
-    static void bfs(int x, int y) {
-        Queue<int[]> q = new LinkedList<>();
-        // 큐에서 꺼내서 방문 처리 
-        q.offer(new int[]{x, y});
-        visited[x][y] = true;
-        
-        // 큐에 남은 요소가 없을 때까지 
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int cx = cur[0];
-            int cy = cur[1];
-            
-            for (int d = 0; d < 8; d++) {
-                int nx = cx + dx[d];
-                int ny = cy + dy[d];
-
-                // 경계 체크
-                if (nx < 0 || ny < 0 || nx >= h || ny >= w) continue;
-               
-                if (visited[nx][ny]) continue; // visited 된 영역은 건너뛰기 
-                if (map[nx][ny] == 0) continue;
-
-                visited[nx][ny] = true;
-                q.offer(new int[]{nx, ny});
-            }
-        }
-    }
+	private static final int[] dr = {-1, -1, -1, 0, 1, 1, 1, 0};
+	private static final int[] dc = {-1, 0, 1, 1, 1, 0, -1, -1};
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int w = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
+		while(w != 0 && h != 0) {
+			int[][] map = new int[h][w];
+			boolean[][] isVisited = new boolean[h][w];
+			for (int i = 0; i < h; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < w; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			int islandCnt = 0;
+			
+			for (int i = 0; i < h; i++) {
+				for (int j = 0; j < w; j++) {
+					if (map[i][j] == 1 && !isVisited[i][j]) {
+						ArrayDeque<int[]> queue = new ArrayDeque<>();
+						queue.add(new int[] {i, j});
+						isVisited[i][j] = true;
+						while(!queue.isEmpty()) {
+							int[] curPos = queue.poll();
+							for (int d = 0; d < 8; d++) {
+								int nr = curPos[0] + dr[d];
+								int nc = curPos[1] + dc[d];
+								if (nr >= 0 && nr < h && nc >= 0 && nc < w
+										&& map[nr][nc] != 0 && !isVisited[nr][nc]) {
+									isVisited[nr][nc] = true;
+									queue.add(new int[] {nr, nc});
+								}
+							}
+						}
+						islandCnt++;
+					}
+				}
+			}
+			
+			sb.append(islandCnt).append('\n');
+			
+			st = new StringTokenizer(br.readLine());
+			w = Integer.parseInt(st.nextToken());
+			h = Integer.parseInt(st.nextToken());
+		}
+		System.out.println(sb);
+	}
 }
