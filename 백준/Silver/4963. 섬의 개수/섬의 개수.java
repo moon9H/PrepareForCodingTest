f13e42e96fd47dc05c0759b4ec2e80060fcbd0ca
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-02-24 16:12:58 +0900

    [Silver II] Title: 섬의 개수, Time: 108 ms, Memory: 14576 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,46 +14,47 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		int w = Integer.parseInt(st.nextToken());
 		int h = Integer.parseInt(st.nextToken());
-		while(w != 0 && h != 0) {
+		
+		while (w + h != 0) {
 			int[][] map = new int[h][w];
-			boolean[][] isVisited = new boolean[h][w];
+			boolean[][] visited = new boolean[h][w];
 			for (int i = 0; i < h; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < w; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
+					if (map[i][j] == 0) visited[i][j] = true;
 				}
 			}
-			int islandCnt = 0;
 			
+			Queue<int[]> queue = new ArrayDeque<>();
+			int island = 0;
 			for (int i = 0; i < h; i++) {
 				for (int j = 0; j < w; j++) {
-					if (map[i][j] == 1 && !isVisited[i][j]) {
-						ArrayDeque<int[]> queue = new ArrayDeque<>();
-						queue.add(new int[] {i, j});
-						isVisited[i][j] = true;
+					if (map[i][j] == 1 && !visited[i][j]) {
+						++island;
+						queue.offer(new int[] {i, j});
+						visited[i][j] = true;
 						while(!queue.isEmpty()) {
 							int[] curPos = queue.poll();
-							for (int d = 0; d < 8; d++) {
-								int nr = curPos[0] + dr[d];
-								int nc = curPos[1] + dc[d];
-								if (nr >= 0 && nr < h && nc >= 0 && nc < w
-										&& map[nr][nc] != 0 && !isVisited[nr][nc]) {
-									isVisited[nr][nc] = true;
-									queue.add(new int[] {nr, nc});
+							for (int k = 0; k < dr.length; k++) {
+								int nr = curPos[0] + dr[k];
+								int nc = curPos[1] + dc[k];
+								if (nr >= 0 && nr < h && nc >= 0 && nc < w && !visited[nr][nc]) {
+									queue.offer(new int[] {nr, nc});
+									visited[nr][nc] = true;
 								}
 							}
 						}
-						islandCnt++;
 					}
 				}
 			}
 			
-			sb.append(islandCnt).append('\n');
-			
+			sb.append(island).append('\n');
 			st = new StringTokenizer(br.readLine());
 			w = Integer.parseInt(st.nextToken());
 			h = Integer.parseInt(st.nextToken());
 		}
+		
 		System.out.println(sb);
 	}
 }
