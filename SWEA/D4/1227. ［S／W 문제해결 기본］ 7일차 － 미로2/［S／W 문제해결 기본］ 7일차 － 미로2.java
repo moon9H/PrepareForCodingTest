ce3e5308df09297d812a700ea2ad284692a03a62
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-02-25 17:49:24 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로2, Time: 106 ms, Memory: 30,808 KB -BaekjoonHub

--- SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.java
+++ SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.java
@@ -5,46 +5,43 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class Solution {
+	private static final int[] dr = {-1, 1, 0, 0};
+	private static final int[] dc = {0, 0, -1, 1};
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = 10;
-		for (int testCase = 1; testCase <= T; testCase++) {
-			// 입력 처리
+		for (int tc = 1; tc <= 10; tc++) {
 			br.readLine();
-			int[][] maze = new int[100][100];
+			int[][] map = new int[100][100];
 			boolean[][] visited = new boolean[100][100];
 			for (int i = 0; i < 100; i++) {
 				String line = br.readLine();
 				for (int j = 0; j < 100; j++) {
-					maze[i][j] = line.charAt(j) - '0';
+					map[i][j] = line.charAt(j) - '0';
 				}
 			}
-			int[] startPos = {1, 1};
-			int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-			int result = 0;
-			Queue<int[]> queue = new ArrayDeque<int[]>();
-			queue.offer(startPos);
+			int isPossible = 0;
+			
+			Queue<int[]> queue = new ArrayDeque<>();
+			queue.add(new int[] {1, 1});
 			visited[1][1] = true;
+			
 			while(!queue.isEmpty()) {
 				int[] curPos = queue.poll();
 				for (int i = 0; i < 4; i++) {
-					int[] nPos = new int[2];
-					nPos[0] = curPos[0] + dir[i][0];
-					nPos[1] = curPos[1] + dir[i][1];
-					if (maze[nPos[0]][nPos[1]] == 0 && !visited[nPos[0]][nPos[1]]) {
-						visited[nPos[0]][nPos[1]] = true;
-						queue.offer(nPos);
-					} else if (maze[nPos[0]][nPos[1]] == 3 && !visited[nPos[0]][nPos[1]]) {
-						result = 1;
-						break;
+					int nr = curPos[0] + dr[i];
+					int nc = curPos[1] + dc[i];
+					if (nr > 0 && nr < 99 && nc > 0 && nc < 99 &&
+						!visited[nr][nc] && map[nr][nc] != 1) {
+						if (map[nr][nc] == 3) isPossible = 1;
+						queue.add(new int[] {nr, nc});
+						visited[nr][nc] = true;
 					}
 				}
-				if (result == 1) break;
 			}
 			
-			sb.append("#").append(testCase).append(" ").append(result).append("\n");
+			sb.append('#').append(tc).append(' ').append(isPossible).append('\n');
 		}
-		System.out.println(sb.toString());
+		System.out.println(sb);
 	}
 }
