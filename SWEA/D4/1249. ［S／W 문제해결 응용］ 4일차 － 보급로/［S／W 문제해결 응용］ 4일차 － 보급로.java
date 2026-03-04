73f10d189663744348b59395442d4a549a357027
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-03-04 15:37:00 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 171 ms, Memory: 35,396 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -5,60 +5,49 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class Solution {
-    private static int N;
-    private static int[][] map;
-    private static final int[] dr = {-1, 1, 0, 0};
-    private static final int[] dc = {0, 0, -1, 1};
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= T; tc++) {
-            // 입력 처리
-            N = Integer.parseInt(br.readLine());
-            map = new int[N][N];
-            int[][] dist = new int[N][N];
-            for (int i = 0; i < N; i++) {
-                Arrays.fill(dist[i], Integer.MAX_VALUE);
-            }
-            PriorityQueue<int[]> pq = new PriorityQueue<>(
-                    (o1, o2) -> o1[2] - o2[2]
-            );
-            for (int i = 0; i < N; i++) {
-                String line = br.readLine();
-                for (int j = 0; j < N; j++) {
-                    map[i][j] = line.charAt(j) - '0';
-                }
-            }
-            // 시작 지점 마킹 및 큐에 add
-            dist[0][0] = map[0][0];
-            pq.add(new int[]{0, 0, map[0][0]});
-            int minRoute = map[0][0];
-            while (!pq.isEmpty()){
-                int[] curPos = pq.poll();
-                int r = curPos[0];
-                int c = curPos[1];
-                int cost = curPos[2];
-
-                if (cost > dist[r][c]) continue;
-
-                if (r == N - 1 && c == N - 1) minRoute = cost;
-
-                for (int i = 0; i < 4; i++) {
-                    int nr = curPos[0] + dr[i];
-                    int nc = curPos[1] + dc[i];
-                    if (nr >= 0 && nr < N && nc >= 0 && nc < N){
-                        int newCost = cost + map[nr][nc];
-                        if (newCost < dist[nr][nc]){
-                            dist[nr][nc] = newCost;
-                            pq.add(new int[] {nr, nc, newCost});
-                        }
-                    }
-                }
-            }
-
-            sb.append('#').append(tc).append(' ').append(minRoute).append('\n');
-        }
-        System.out.println(sb);
-    }
+	private static final int[] dr = {-1, 1, 0, 0};
+	private static final int[] dc = {0, 0, -1, 1};
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			int[][] map = new int[N][N];
+			int[][] dist = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				String line = br.readLine();
+				for (int j = 0; j < N; j++) {
+					map[i][j] = line.charAt(j) - '0';
+				}
+				Arrays.fill(dist[i], Integer.MAX_VALUE);
+			}
+			
+			PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[0], o2[0]));
+			
+			dist[0][0] = map[0][0];
+			pq.add(new int[] {map[0][0], 0, 0});
+			
+			while(!pq.isEmpty()) {
+				int[] curPos = pq.poll();
+				
+				for (int d = 0; d < 4; d++) {
+					int nr = curPos[1] + dr[d];
+					int nc = curPos[2] + dc[d];
+					
+					if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+					
+					int newCost = map[nr][nc] + curPos[0];
+					
+					if (dist[nr][nc] > newCost) {
+						dist[nr][nc] = newCost;
+						pq.add(new int[] {newCost, nr, nc});
+					}
+				}
+			}
+			
+			sb.append('#').append(tc).append(' ').append(dist[N - 1][N - 1]).append('\n');
+		}
+		System.out.println(sb);
+	}
 }
