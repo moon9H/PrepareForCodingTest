af220b532d0ea0f0dee19bb3d644b4c2fe852235
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-03-05 16:30:54 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 216 ms, Memory: 34,816 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -11,37 +11,43 @@ public class Solution {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
+		
 		for (int tc = 1; tc <= T; tc++) {
 			int N = Integer.parseInt(br.readLine());
 			int[][] map = new int[N][N];
 			int[][] dist = new int[N][N];
+			for (int[] is : dist) {
+				Arrays.fill(is, Integer.MAX_VALUE);
+			}
+			
 			for (int i = 0; i < N; i++) {
 				String line = br.readLine();
 				for (int j = 0; j < N; j++) {
 					map[i][j] = line.charAt(j) - '0';
 				}
-				Arrays.fill(dist[i], Integer.MAX_VALUE);
 			}
 			
-			PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[0], o2[0]));
-			
 			dist[0][0] = map[0][0];
-			pq.add(new int[] {map[0][0], 0, 0});
+			PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+				return Integer.compare(o1[2], o2[2]);
+			});
+			
+			pq.add(new int[] {0, 0, dist[0][0]});
 			
 			while(!pq.isEmpty()) {
 				int[] curPos = pq.poll();
-				
+				if (curPos[2] > dist[curPos[0]][curPos[1]]) continue;
 				for (int d = 0; d < 4; d++) {
-					int nr = curPos[1] + dr[d];
-					int nc = curPos[2] + dc[d];
+					int nr = curPos[0] + dr[d];
+					int nc = curPos[1] + dc[d];
 					
 					if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
 					
-					int newCost = map[nr][nc] + curPos[0];
+					int newCost = curPos[2] + map[nr][nc];
 					
 					if (dist[nr][nc] > newCost) {
 						dist[nr][nc] = newCost;
-						pq.add(new int[] {newCost, nr, nc});
+						pq.add(new int[] {nr, nc, newCost});
 					}
 				}
 			}
