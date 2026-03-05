1d2f508a30b1424942460e4d19849f7e0bb7d158
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-03-05 15:18:30 +0900

    [D2] Title: 나무 높이, Time: 95 ms, Memory: 27,008 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -5,54 +5,53 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= T ; tc++) {
-            int N = Integer.parseInt(br.readLine());
-            int[] trees = new int[N];
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) {
-                trees[i] = Integer.parseInt(st.nextToken());
-            }
-
-            Arrays.sort(trees);
-
-            int minDays = Integer.MAX_VALUE;
-
-            for (int target = trees[N - 1]; target <= trees[N - 1] + 1; target++) {
-                int even = 0, odd = 0;
-                int ans = 0;
-
-                for (int i = 0; i < N - 1; i++) {
-                    int residual = target - trees[i];
-                    if (residual > 0){
-                        even += residual / 2;
-                        odd += residual % 2;
-                    }
-                }
-
-                if (even > odd){
-                    while (Math.abs(even - odd) > 1){
-                        even--;
-                        odd += 2;
-                    }
-                }
-
-                if (odd > even){
-                    ans = 2 * odd - 1;
-                } else if (even > odd){
-                    ans = 2 * even;
-                } else {
-                    ans = even + odd;
-                }
-
-                minDays = Math.min(minDays, ans);
-            }
-
-            sb.append('#').append(tc).append(' ').append(minDays).append('\n');
-        }
-        System.out.println(sb);
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int tc = 1; tc <= T; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			int[] treeHeights = new int[N];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				treeHeights[i] = Integer.parseInt(st.nextToken());
+			}
+			Arrays.sort(treeHeights);
+			
+			int minDay = Integer.MAX_VALUE;
+			for (int target = treeHeights[N - 1]; target <= treeHeights[N - 1] + 1; target++) {
+				int evenDiff = 0, oddDiff = 0;
+				int ans = 0;
+				
+				for (int i = 0; i < N - 1; i++) {
+					int residual = target - treeHeights[i];
+					if (residual > 0) {
+						evenDiff += residual / 2;
+						oddDiff += residual % 2;
+					}
+				}
+				
+				if (evenDiff > oddDiff) {
+					while(Math.abs(evenDiff - oddDiff) > 1) {
+						evenDiff--;
+						oddDiff += 2;
+					}
+				}
+				
+				if (oddDiff > evenDiff) {
+					ans = 2 * oddDiff - 1;
+				} else if (evenDiff > oddDiff) {
+					ans = 2 * evenDiff;
+				} else {
+					ans  = oddDiff + evenDiff;
+				}
+				
+				minDay = Math.min(minDay, ans);
+			}
+			sb.append('#').append(tc).append(' ').append(minDay).append('\n');
+		}
+		
+		System.out.println(sb);
+	}
 }
