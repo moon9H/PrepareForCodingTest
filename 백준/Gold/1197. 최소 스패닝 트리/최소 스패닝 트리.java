242d3e4cd0109390cd688e90947ac4fb5808a4ed
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-03-10 15:01:58 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 640 ms, Memory: 52756 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,64 +1,79 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	private static int V;
-	private static int[] parents;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		V = Integer.parseInt(st.nextToken());
-		makeSets();
-		ArrayList<int[]> edgeGraph = new ArrayList<>();
-		int E = Integer.parseInt(st.nextToken());
-		for (int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			edgeGraph.add(new int[] {a, b, c});
-		}
-		edgeGraph.sort((o1, o2) -> {
-			return Integer.compare(o1[2], o2[2]);
-		});
-		
-		int cnt = 0;
-		int weight = 0;
-		for (int[] edge : edgeGraph) {
-			if (union(edge[0], edge[1])) {
-				++cnt;
-				weight += edge[2];
-				if (cnt == V - 1) break;
-			}
-		}
-		
-		System.out.println(weight);
-	}
-	
-	static void makeSets() {
-		parents = new int[V + 1];
-		for (int i = 0; i <= V; i++) {
-			parents[i] = i;
-		}
-	}
-	
-	static int findSet(int a) {
-		if (parents[a] == a) return a;
-		return parents[a] = findSet(parents[a]);
-	}
-	
-	static boolean union(int a, int b) {
-		int aRoot = findSet(a);
-		int bRoot = findSet(b);
-		
-		if (aRoot == bRoot) return false;
-		
-		parents[bRoot] = aRoot;
-		
-		return true;
-	}
+    static int[] parent;
+    static int[] rank;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());   // 정점의 개수 
+        int E = Integer.parseInt(st.nextToken());   // 간선의 개수
+
+        Edge[] edges = new Edge[E];
+        // 1. edges 배열에 간선 정보(가중치)를 저장한다.
+        // 2. edges 배열을 weight 기준으로 정렬한다.
+        // 3. unionFind에 필요한 parent 배열을 만든다.
+        // 4. edges 배열에서 하나씩 순차적으로 꺼내며 union을 수행한다.
+        //    이때 이미 같은 그룹인 경우 false를 리턴하고 union을 수행한 경우 true를 리턴한다.
+        //    true를 받을 경우 간선수를 추가하고 최종 가중치를 더한다.
+        // 5. 연결된 간선의 수가 V-1인경우 최종 가중치를 출력한다.
+
+        for(int i = 0;i<E;i++) {
+            StringTokenizer now = new StringTokenizer(br.readLine());
+            int nowStart = Integer.parseInt(now.nextToken());
+            int nowEnd = Integer.parseInt(now.nextToken());
+            int nowWeight = Integer.parseInt(now.nextToken());
+            Edge edge = new Edge(nowStart, nowEnd, nowWeight);
+            edges[i] = edge;
+        }
+
+        Arrays.sort(edges, (a, b) -> a.weight - b.weight);
+
+        parent = new int[V+1];
+        for(int i = 0;i<=V;i++) {
+            parent[i] = i;
+        }
+
+        int totalEdges = 0;
+        int totalWeight = 0;
+        for(Edge e : edges) {
+            boolean isLoop = union(e.start, e.end);
+            if(!isLoop) {
+                totalEdges++;
+                totalWeight += e.weight;
+            }
+            if(totalEdges == V - 1) break;
+        }
+
+        System.out.println(totalWeight);
+    }
+
+    static boolean union (int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+        if(rootA == rootB) return true;
+        parent[rootB] = rootA;
+        return false;
+    }
+
+    static int find (int a) {
+        if(parent[a] != a) {
+            parent[a] = find(parent[a]);
+        }
+        return parent[a];
+    }
+
+    static class Edge {
+        int start;
+        int end;
+        int weight;
+        public Edge() {}
+        public Edge(int start, int end, int weight) {
+            this.start = start;
+            this.end = end;
+            this.weight = weight;
+        }
+    }
 }
