5631325fe475f881329b15d4dcd0668e7e83a248
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-03-11 13:38:17 +0900

    [D4] Title: 최소 스패닝 트리, Time: 2,691 ms, Memory: 200,804 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,68 +2,59 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
- 
+
 public class Solution {
-    private static int V;
-    private static int[] parents;
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= T; tc++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            V = Integer.parseInt(st.nextToken());
-            makeSets();
-            int E = Integer.parseInt(st.nextToken());
-            ArrayList<int[]> edgeGraph = new ArrayList<>();
-            for (int i = 0; i < E; i++) {
-                st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                int c = Integer.parseInt(st.nextToken());
-                 
-                edgeGraph.add(new int[] {a, b, c});
-            }
-             
-            edgeGraph.sort((o1, o2) -> {
-                return Integer.compare(o1[2], o2[2]);
-            });
-             
-            int cnt = 0;
-            long result = 0;
-            for (int[] edge : edgeGraph) {
-                if (union(edge[0], edge[1])) {
-                    ++cnt;
-                    result += edge[2];
-                    if (cnt == V - 1) break;
-                }
-            }
-             
-            sb.append('#').append(tc).append(' ').append(result).append('\n');
-        }
-        System.out.println(sb);
-    }
-     
-    static void makeSets() {
-        parents = new int[V + 1];
-        for (int i = 0; i <= V; i++) {
-            parents[i] = i;
-        }
-    }
-     
-    static int findSet(int a) {
-        if (a == parents[a]) return a;
-        return parents[a] = findSet(parents[a]); 
-    }
-     
-    static boolean union(int a, int b) {
-        int aRoot = findSet(a);
-        int bRoot = findSet(b);
-         
-        if (aRoot == bRoot) return false;
-         
-        parents[bRoot] = aRoot;
-        return true;
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int V = Integer.parseInt(st.nextToken());
+			int E = Integer.parseInt(st.nextToken());
+			boolean[] visited = new boolean[V + 1];
+			int[] minEdge = new int[V + 1];
+			ArrayList<int[]>[] graph = new ArrayList[V + 1];
+			for (int i = 0; i <= V; i++) {
+				graph[i] = new ArrayList<>();
+			}
+			
+			for (int i = 0; i < E; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+				graph[a].add(new int[] {b, c});
+				graph[b].add(new int[] {a, c});
+			}
+			
+			PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+				return Integer.compare(o1[1], o2[1]);
+			});
+			pq.add(new int[] {1, 0});
+			long result = 0;
+			int count = 0;
+			
+			while(!pq.isEmpty()) {
+				int[] curNode = pq.poll();
+				
+				if (visited[curNode[0]]) continue;
+				
+				visited[curNode[0]] = true;
+				result += curNode[1];
+				++count;
+				
+				for (int[] nextNode : graph[curNode[0]]) {
+					if (!visited[nextNode[0]]) {
+						pq.add(new int[] {nextNode[0], nextNode[1]});
+					}
+				}
+			}
+			
+			sb.append('#').append(tc).append(' ').append(count == V ? result : -1).append('\n');
+		}
+		System.out.println(sb);
+	}
 }
