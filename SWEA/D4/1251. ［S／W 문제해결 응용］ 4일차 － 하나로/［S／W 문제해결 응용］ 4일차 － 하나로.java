24d4d1b1127b9ae578e465d0f175f5aa8d584e11
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-03-11 14:02:39 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 997 ms, Memory: 157,868 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -2,23 +2,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	private static int N;
-	private static int[] parents;
-	static class Edge{
-		int row, col;
-		double distance;
-		public Edge(int row, int col, double distance) {
+	static class Node{
+		int to;
+		double weight;
+		public Node(int to, double weight) {
 			super();
-			this.row = row;
-			this.col = col;
-			this.distance = distance;
-		}
-		@Override
-		public String toString() {
-			return "Edge [row=" + row + ", col=" + col + ", distance=" + distance + "]";
+			this.to = to;
+			this.weight = weight;
 		}
 	}
 	public static void main(String[] args) throws IOException{
@@ -26,62 +20,56 @@ public class Solution {
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
-			N = Integer.parseInt(br.readLine());
-			makeSet();
-			int[][] islandArr = new int[N][2];
+			int N = Integer.parseInt(br.readLine());
 			StringTokenizer st = new StringTokenizer(br.readLine());
+			int[][] islandArr = new int[N][2]; 
+			ArrayList<Node>[] graph = new ArrayList[N];
+			boolean[] visited = new boolean[N];
 			for (int i = 0; i < N; i++) {
-				islandArr[i][0] = Integer.parseInt(st.nextToken()); 
+				graph[i] = new ArrayList<>();
+			}
+			for (int i = 0; i < N; i++) {
+				islandArr[i][0] = Integer.parseInt(st.nextToken());
 			}
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++) {
-				islandArr[i][1] = Integer.parseInt(st.nextToken()); 
+				islandArr[i][1] = Integer.parseInt(st.nextToken());
 			}
-			
 			double E = Double.parseDouble(br.readLine());
-			ArrayList<Edge> edgeGraph = new ArrayList<>();
 			for (int i = 0; i < N - 1; i++) {
 				for (int j = i + 1; j < N; j++) {
-					edgeGraph.add(new Edge(i, j, Math.sqrt(Math.pow(islandArr[i][0] - islandArr[j][0], 2) + 
-												 			Math.pow(islandArr[i][1] - islandArr[j][1], 2))));
+					graph[i].add(new Node(j, Math.sqrt(Math.pow(islandArr[i][0] - islandArr[j][0], 2) 
+														+ Math.pow(islandArr[i][1] - islandArr[j][1], 2))));
+					graph[j].add(new Node(i, Math.sqrt(Math.pow(islandArr[i][0] - islandArr[j][0], 2) 
+							+ Math.pow(islandArr[i][1] - islandArr[j][1], 2))));
 				}
 			}
-			edgeGraph.sort((o1, o2) -> {
-				return Double.compare(o1.distance, o2.distance);
+			
+			PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> {
+				return Double.compare(o1.weight, o2.weight);
 			});
+			pq.add(new Node(1, 0));
 			
 			double result = 0;
 			int count = 0;
-			
-			for (Edge edge : edgeGraph) {
-				if (union(edge.row, edge.col)) {
-					++count;
-					result += Math.pow(edge.distance, 2) * E;
-					if (count == N - 1) break;
+			while(!pq.isEmpty()) {
+				Node curNode = pq.poll();
+				
+				if (visited[curNode.to]) continue;
+				
+				++count;
+				result += curNode.weight * curNode.weight * E;
+				visited[curNode.to] = true;
+				
+				for (Node nextNode : graph[curNode.to]) {
+					if (!visited[nextNode.to]) {
+						pq.add(new Node(nextNode.to, nextNode.weight));
+					}
 				}
 			}
+			
 			sb.append('#').append(tc).append(' ').append(Math.round(result)).append('\n');
 		}
 		System.out.println(sb);
-	}
-	
-	static void makeSet() {
-		parents = new int[N];
-		for (int i = 0; i < N; i++) {
-			parents[i] = i;
-		}
-	}
-	
-	static int findSet(int a) {
-		if (parents[a] == a) return a;
-		return parents[a] = findSet(parents[a]);
-	}
-	
-	static boolean union(int a, int b) {
-		int aRoot = findSet(a);
-		int bRoot = findSet(b);
-		if (aRoot == bRoot) return false;
-		parents[bRoot] = aRoot;
-		return true;
 	}
 }
