21d70f88d1294c86b1c60cd50a99e58693e0d506
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-03-11 17:38:59 +0900

    [Gold IV] Title: 키 순서, Time: 424 ms, Memory: 35724 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -1,64 +1,56 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		ArrayList<Integer>[] graph = new ArrayList[N + 1];
-		ArrayList<Integer>[] reverseGraph = new ArrayList[N + 1];
-		for (int i = 0; i <= N; i++) {
-			graph[i] = new ArrayList<>();
-			reverseGraph[i] = new ArrayList<>();
-		}
-		
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			
-			graph[from].add(to);
-			reverseGraph[to].add(from);
-		}
-		
-		int ans = 0;
-		for (int i = 1; i <= N; i++) {
-			int in = bfs(graph, i, N);
-			int out = bfs(reverseGraph, i, N);
-			
-			if (in + out == N - 1) ++ans;
-		}
-		
-		System.out.println(ans);
-	}
-	
-	static int bfs(ArrayList<Integer>[] graph, int start, int size) {
-		int count = 0;
-		boolean[] visited = new boolean[size + 1];
-		Queue<Integer> queue = new ArrayDeque<>();
-		visited[start] = true;
-		queue.add(start);
-		
-		while(!queue.isEmpty()) {
-			int curNode = queue.poll();
-			
-			for (Integer integer : graph[curNode]) {
-				if (!visited[integer]) {
-					++count;
-					queue.add(integer);
-					visited[integer] = true;
-				}
-			}
-		}
-				
-		return count;
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        // row가 col보다 작은지 저장하는 배열
+        boolean[][] board = new boolean[N + 1][N + 1];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            board[a][b] = true; // a < b
+        }
+
+        // 플로이드 워셜로 간접 비교 관계 갱신
+        // i : 출발점, k : 중간점, j : 도착점
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+
+                // i에서 k로 갈 수 없을 경우 continue
+                if (!board[i][k]) continue;
+                
+                // i에서 k를 거쳐 j로 갈 수 있을 경우 i에서 j로 갈 수 있는것으로 변경
+                for (int j = 1; j <= N; j++) {
+                    if (board[k][j]) {
+                        board[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        int answer = 0;
+
+        for (int i = 1; i <= N; i++) {
+            int count = 0;
+
+            // i와 j가 어떻게든 연결되어있을경우 (비교가 가능 할 경우) count를 증가
+            for (int j = 1; j <= N; j++) {
+                if (i == j) continue;
+                if (board[i][j] || board[j][i]) count++;
+            }
+
+            // count가 N-1일경우 i는 모든 항목과 연결된것이므로 키가 몇번째인지 판단할 수 있음
+            if (count == N - 1) answer++;
+        }
+
+        System.out.println(answer);
+    }
 }
