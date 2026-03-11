8447739bd6ef073506e302156994261a830426eb
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-03-11 15:13:55 +0900

    [Gold IV] Title: 미로만들기, Time: 220 ms, Memory: 19240 KB -BaekjoonHub

---
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] dr = {0,1,0,-1};
+    static int[] dc = {1,0,-1,0};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(br.readLine());
+        
+        int[][] board = new int[size][size];
+        int[][] dist = new int[size][size];
+        for(int i = 0;i<size;i++) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
+        }
+
+        for(int i = 0;i<size;i++){
+            int[] row = Arrays.stream(br.readLine().split("")).mapToInt(Integer::valueOf).toArray();
+            for(int j = 0;j<size;j++){
+                board[i][j] = row[j];
+            }
+        }
+
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.weight - b.weight);
+
+        pq.add(new Node(0, 0, 0));
+        dist[0][0] = 0;
+
+        while(!pq.isEmpty()) {
+            Node now = pq.poll();
+
+            if(now.weight != dist[now.r][now.c]) continue;
+            
+            for(int i = 0;i<4;i++) {
+                int nextR = now.r + dr[i];
+                int nextC = now.c + dc[i];
+                if(nextR >= 0 && nextC >= 0 && nextR < size && nextC < size) {
+                    // 진행방향의 기존 weight보다 현재 연산하는 weight가 더 작을경우 queue에 추가한다.
+                    int nextCost = dist[now.r][now.c];
+                    if(board[nextR][nextC] == 0) {
+                        nextCost += 1;    
+                    }
+                    if(dist[nextR][nextC] > nextCost) {
+                        dist[nextR][nextC] = nextCost;
+                        pq.add(new Node(nextR, nextC, nextCost));
+                    }
+                }
+            }
+        }
+
+        System.out.println(dist[size-1][size-1]);
+
+    }
+
+    static class Node {
+        int r;
+        int c;
+        int weight;
+        public Node(int r, int c, int weight) {
+            this.r = r;
+            this.c = c;
+            this.weight = weight;
+        }
+    }
+}
