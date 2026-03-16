9dd36bd3f36c85bc0b3d0b3ebffe79dd077aa07f
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-03-16 11:16:16 +0900

    [Gold III] Title: 파티, Time: 276 ms, Memory: 21352 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -6,89 +6,76 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main{
-	public static void main(String[] args) throws IOException{
-		//------여기에 솔루션 코드를 작성하세요.------------
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int X = Integer.parseInt(st.nextToken());
-		
-		@SuppressWarnings("unchecked")
-		ArrayList<int[]>[] graph = new ArrayList[N + 1];
-		for (int i = 0; i <= N; i++) {
-			graph[i] = new ArrayList<>();
-		}
-		
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int t = Integer.parseInt(st.nextToken());
-			graph[a].add(new int[] {b, t});
-		}
-		
-		int longTakes = 0;
-		
-		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
-		int[] distX = new int[N + 1];
-		Arrays.fill(distX, Integer.MAX_VALUE);
-		boolean[] visited = new boolean[N + 1];
-		distX[X] = 0;
-		pq.add(new int[] {X, 0});
-		
-		while(!pq.isEmpty()) {
-			int[] curNode = pq.poll();
-			
-			if (visited[curNode[0]]) continue;
-			
-			visited[curNode[0]] = true;
-			
-			for (int[] nextNode : graph[curNode[0]]) {
-				int newCost = curNode[1] + nextNode[1];
-				
-				if (distX[nextNode[0]] > newCost) {
-					distX[nextNode[0]] = newCost;
-					pq.add(new int[] {nextNode[0], newCost});
-				}
-			}
-		}
-		
-		
-		for (int i = 1; i <= N; i++) {
-			if (i == X) continue;
-			
-			int time = 0;
-			int[] dist = new int[N + 1];
-			Arrays.fill(visited, false);
-			Arrays.fill(dist, Integer.MAX_VALUE);
-			
-			dist[i] = 0;
-			pq.add(new int[] {i, 0});
-			
-			while(!pq.isEmpty()) {
-				int[] curNode = pq.poll();
-				
-				if (visited[curNode[0]]) continue;
-				
-				visited[curNode[0]] = true;
-				
-				for (int[] nextNode : graph[curNode[0]]) {
-					int newCost = curNode[1] + nextNode[1];
-					
-					if (dist[nextNode[0]] > newCost) {
-						dist[nextNode[0]] = newCost;
-						pq.add(new int[] {nextNode[0], newCost});
-					}
-				}
-			}
-			
-			time = dist[X] + distX[i];
-			
-			longTakes = Math.max(time, longTakes);
-		}
-		
-		System.out.println(longTakes);
-	}
+public class Main {
+    private static final int INF = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        @SuppressWarnings("unchecked")
+        ArrayList<int[]>[] graph = new ArrayList[N + 1];
+        @SuppressWarnings("unchecked")
+        ArrayList<int[]>[] reverseGraph = new ArrayList[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+            reverseGraph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+
+            graph[a].add(new int[] {b, t});
+            reverseGraph[b].add(new int[] {a, t}); // 역방향
+        }
+
+        int[] go = dijkstra(graph, X, N);         // X -> i
+        int[] back = dijkstra(reverseGraph, X, N); // i -> X
+
+        int longTakes = 0;
+        for (int i = 1; i <= N; i++) {
+            longTakes = Math.max(longTakes, go[i] + back[i]);
+        }
+
+        System.out.println(longTakes);
+    }
+
+    private static int[] dijkstra(ArrayList<int[]>[] graph, int start, int N) {
+        int[] dist = new int[N + 1];
+        boolean[] visited = new boolean[N + 1];
+        Arrays.fill(dist, INF);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
+        dist[start] = 0;
+        pq.add(new int[] {start, 0});
+
+        while (!pq.isEmpty()) {
+            int[] curNode = pq.poll();
+            int cur = curNode[0];
+            int cost = curNode[1];
+
+            if (visited[cur]) continue;
+            visited[cur] = true;
+
+            for (int[] nextNode : graph[cur]) {
+                int next = nextNode[0];
+                int nextCost = nextNode[1];
+
+                if (dist[next] > cost + nextCost) {
+                    dist[next] = cost + nextCost;
+                    pq.add(new int[] {next, dist[next]});
+                }
+            }
+        }
+
+        return dist;
+    }
 }
