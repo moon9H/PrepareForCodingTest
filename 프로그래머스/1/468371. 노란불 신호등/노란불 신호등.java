63d1dd9d3e8d3435abcaab8c7f51157e3e01edfc
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-03-17 15:10:17 +0900

    [level 1] Title: 노란불 신호등, Time: 11.34 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
@@ -1,63 +1,36 @@
 class Solution {
-    static class Light{
-		int[] lightTime = new int[3];
-		int status;
-		int cycle;
-		public Light(int green, int yellow, int red) {
-			super();
-			this.lightTime[0] = green;
-			this.lightTime[1] = yellow;
-			this.lightTime[2] = red;
-			this.status = 0;
-			this.cycle = green + yellow + red;
-		}
-	}
-	
-	public int solution(int[][] signals) {
+    public int solution(int[][] signals) {
         int answer = -1;
-        Light[] lights = new Light[signals.length];
-        int[] sum = new int[signals.length];
+        int[] cycle = new int[signals.length];
         for (int i = 0; i < signals.length; i++) {
-        	lights[i] = new Light(signals[i][0], signals[i][1], signals[i][2]);
-        	sum[i] += signals[i][0] + signals[i][1] + signals[i][2];
+        	cycle[i] += signals[i][0] + signals[i][1] + signals[i][2];
 		}
         
-        int LCM = lcmArray(sum);
+        int LCM = lcmArray(cycle);
         
-        int t = 0;
-        while (t < LCM) {
-        	boolean isAllYellow = true;
-        	for (int i = 0; i < lights.length; i++) {
-				if (lights[i].status != 1) {
+        for (int t = 0; t < LCM; t++) {
+			boolean isAllYellow = true;
+			
+			for (int i = 0; i < signals.length; i++) {
+				int time = t % cycle[i];
+				int green = signals[i][0];
+				int yellow = signals[i][1];
+				
+				if (! (green <= time && time < green + yellow)) {
 					isAllYellow = false;
 					break;
 				}
 			}
-        	
-        	if (isAllYellow) {
-        		answer = t + 1;
-        		break;
-        	}
-        	
-        	++t;
-        	
-        	for (Light light : lights) {
-				int cycleTime = t % light.cycle;
-				
-				if (cycleTime < light.lightTime[0]) {
-			        light.status = 0; // green
-			    } else if (cycleTime < light.lightTime[0] + light.lightTime[1]) {
-			        light.status = 1; // yellow
-			    } else {
-			        light.status = 2; // red
-			    }
+			
+			if (isAllYellow) {
+				answer = t + 1;
+				break;
 			}
-        	
-        }
+		}
         
         return answer;
     }
-    
+
     public static int lcmArray(int[] arr) {
         int result = arr[0];
         for (int i = 1; i < arr.length; i++) {
