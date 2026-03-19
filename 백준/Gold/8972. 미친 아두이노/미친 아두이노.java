3e7452581558727fb4ba84e4531f2b26e4b9f098
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-03-19 14:15:42 +0900

    [Gold III] Title: 미친 아두이노, Time: 540 ms, Memory: 25688 KB -BaekjoonHub

--- 백준/Gold/8972. 미친 아두이노/미친 아두이노.java
+++ 백준/Gold/8972. 미친 아두이노/미친 아두이노.java
@@ -1,121 +1,102 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static int R, C, Jrow, Jcol, cnt;
-	public static char[][] map;
-	public static int[] Jmove;
-	public static List<int[]> list;
-	public static int[] dx = {1, 1, 1, 0, 0, 0, -1, -1, -1};
-	public static int[] dy = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
-	
-	public static void main(String[] args) throws IOException {
+	static int[] dr = {0,1,1,1,0,0,0,-1,-1,-1};
+	static int[] dc = {0,-1,0,1,-1,0,1,-1,0,1};
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		map = new char[R][C];
-		list = new ArrayList<>();
-		for(int i = 0; i < R; i++) {
-			String str = br.readLine();
-			map[i] = str.toCharArray();
-			for(int j = 0; j < C; j++) {
-				if(map[i][j] == 'I') {
-					Jrow = i;
-					Jcol = j;
-				} 
-				if(map[i][j] == 'R') {
-					list.add(new int[] {i, j});
+		int R = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+
+		pos J = new pos(0, 0);      // 종수 아두이노 위치
+		List<pos> A = new ArrayList<>(); // 미친 아두이노들의 위치
+
+		for(int i=0;i<R;i++) {
+			String row = br.readLine();
+			for(int j=0;j<C;j++) {
+                char ch = row.charAt(j);
+				if(ch == 'I') {
+					J.r = i;
+					J.c = j;
+				} else if(ch == 'R') {
+					A.add(new pos(i, j));
 				}
 			}
-		}
-		char[] tmp = br.readLine().toCharArray();
-		Jmove = new int[tmp.length];
-		for(int i = 0; i < tmp.length; i++) {
-			Jmove[i] = (tmp[i]-'0') - 1;
-		}
-		if(move()) {
-			for(int i = 0; i < R; i++) {
-				for(int j = 0; j < C; j++) {
-					System.out.print(map[i][j]);					
-				}
-				System.out.println();
-			}
-		} else {
-			System.out.println("kraj " + cnt);
 		}
 		
-	}
-	
-	// 종수와 아두이노의 거리 계산
-	private static int getDistance(int Ar, int Ac) {
-		return Math.abs(Jrow - Ar) + Math.abs(Jcol - Ac);
-	}
-	
-	// 종수 이동
-	public static boolean move() {
-		for(int i = 0; i < Jmove.length; i++) {
-			int nr = Jrow+dx[Jmove[i]];
-			int nc = Jcol+dy[Jmove[i]];
-			cnt++;
-			for(int[] r : list) {
-			    if(r[0] == nr && r[1] == nc) return false;
-			}
-			Jrow = nr;
-			Jcol = nc;
-			List<int[]> dupli = new ArrayList<>();
-			int[][] Copymap = new int[R][C];
-			
-			// 아두이노 이동
-			for(int k = 0; k < list.size(); k++) {
-				int min = Integer.MAX_VALUE;
-				int curR = list.get(k)[0];
-				int curC = list.get(k)[1];
-				int minR = 0;
-				int minC = 0;
-				for(int j = 0; j < 9; j++) {
-					int Ar = curR + dx[j];
-					int Ac = curC + dy[j];
-					if(Ar < 0 || Ar >= R || Ac < 0 || Ac >= C) continue;
-					// 최소방향 탐색
-					int tmp = getDistance(Ar, Ac);
-					// 최소방향이면 minR, minC 갱신
-					if(min > tmp) {
-						min = tmp;
-						minR = Ar;
-						minC = Ac;
-					}
-				}
-				// 최소거리에 copymap +1 추가
-				Copymap[minR][minC]++;
-			}
-			for(int j = 0; j < R; j++) {
-				for(int k = 0; k < C; k++) {
-					if(Copymap[j][k] >= 1) {
-						if(j == Jrow && k == Jcol) return false;
-					}
-					if(Copymap[j][k] == 1) {
-						dupli.add(new int[] {j, k});
-					}
+        String move = br.readLine();
+		// int[] move = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+		
+		int kraj = 0;
+		for(int m = 0;m<move.length();m++) {
+            // 종수 아두이노 이동
+            int dir = move.charAt(m) - '0';
+			J.r += dr[dir];
+			J.c += dc[dir];
+			kraj++;
+
+			for(pos p : A) {    // 종수 아두이노 이동후 충돌 판정
+                if(p.d) continue;
+				if(J.r == p.r && J.c == p.c) {
+                    System.out.printf("kraj %d", kraj);
+					return;
 				}
 			}
-			list = dupli;
-			
-			// 다시 초기화
-			for(int r = 0; r < R; r++) {
-				for(int c = 0; c < C; c++){
-					map[r][c] = '.';
-				}
+
+			for(pos p : A) {    // 미친 아두이노 이동
+                if(p.d) continue;
+                p.r -= Integer.compare(p.r - J.r, 0);
+                p.c -= Integer.compare(p.c - J.c, 0);
+
+                if(p.r == J.r && p.c == J.c) {  // 각 아두이노 이동후 개별적으로 충돌 판정
+                    System.out.printf("kraj %d", kraj);
+                    return;
+                }
 			}
-			map[nr][nc] = 'I';
-			for(int[] r : list) {
-				map[r[0]][r[1]] = 'R';
-			}
+
+            int[][] isCrash = new int[R][C];    // 특정 위치에 몇개의 미친 아두이노가 있는지 카운트
+            for(pos p : A) {
+                if(p.d) continue;
+                isCrash[p.r][p.c]++;
+            }
+
+            for(int i = 0;i<R;i++) {    // 카운트 배열을 순회하며 2이상인 칸의 아두이노 제거
+                for(int j = 0;j<C;j++) {
+                    if(isCrash[i][j] > 1) {
+                        for(pos p : A) {
+                            if(p.r == i && p.c == j) p.d = true;
+                        }
+                    }
+                }
+            }
 		}
-		return true;
+
+        char[][] answer = new char[R][C];
+        for(int i = 0;i<R;i++) {
+            Arrays.fill(answer[i], '.');
+        }
+        answer[J.r][J.c] = 'I';
+        for(pos p : A) {
+            if(p.d) continue;
+            answer[p.r][p.c] = 'R';
+        }
+        for(int i = 0;i<R;i++) {
+            for(int j = 0;j<C;j++) {
+                System.out.printf("%c", answer[i][j]);
+            }
+            System.out.println();
+        }
+	}
+
+	static class pos {
+		int r;
+		int c;
+        boolean d = false;
+		pos(int r, int c){
+			this.r = r;
+			this.c = c;
+		}
 	}
 }
