a49bba9f4424948f93d18874a5ffc8b388164e32
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-03-22 11:08:02 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 136ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260322/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static final int[] dr = {-1, 1, 0, 0};
+    private static final int[] dc = {0, 0, -1, 1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] map = new int[N][M];
+        boolean[][] visited = new boolean[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int ans = 0;
+
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[] {0, 0});
+        visited[0][0] = true;
+
+        while(!queue.isEmpty()) {
+            int[] curPos = queue.poll();
+            if (curPos[0] == N - 1 && curPos[1] == M - 1) ans = 1;
+            for (int d = 0; d < 4; d++) {
+                int nr = curPos[0] + dr[d];
+                int nc = curPos[1] + dc[d];
+
+                if (nr < 0 || nr >= N || nc < 0 || nc >= M || visited[nr][nc] || map[nr][nc] == 0) continue;
+
+                visited[nr][nc] = true;
+                queue.add(new int[] {nr, nc});
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(ans);
+        System.out.println(sb);
+    }
+}
