19aa1e3c23b5d75809325314ab5b7fa8bf4253a8
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-03-23 21:53:52 +0900

    [Gold IV] Title: 알고스팟, Time: 80 ms, Memory: 12184 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,58 +1,53 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] dr = {0,1,0,-1};
-    static int[] dc = {1,0,-1,0};
+    private static final int[] dr = {-1, 1, 0, 0};
+    private static final int[] dc = {0, 0, -1, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int sizeC = Integer.parseInt(st.nextToken());
-        int sizeR = Integer.parseInt(st.nextToken());
-        int[][] board = new int[sizeR][sizeC];
-        
-        for(int i = 0;i<sizeR;i++) {
-            board[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::valueOf).toArray();
-        }
-
-        int[][] minDist = new int[sizeR][sizeC];
-        for(int i = 0;i<sizeR;i++) {
-            Arrays.fill(minDist[i], Integer.MAX_VALUE);
-        }
-        minDist[0][0] = 0;
-
-		Deque<pos> dq = new ArrayDeque<>();
-        dq.addFirst(new pos(0, 0));
-        while(!dq.isEmpty()) {
-            pos now = dq.pollFirst();
-
-            for(int i = 0;i<4;i++) {
-                int nextR = now.r + dr[i];
-                int nextC = now.c + dc[i];
-                if(nextR < 0 || nextC < 0 || nextR >= sizeR || nextC >= sizeC) continue;
-                int nextW = minDist[now.r][now.c] + board[nextR][nextC];
-                if(nextW >= minDist[nextR][nextC]) continue;
-
-				// 보드의 다음값이 0이면 deque의 앞에 추가
-				// 보드의 다음값이 1이면 deque의 뒤에 추가
-                minDist[nextR][nextC] = nextW;
-				if(board[nextR][nextC] == 0) {
-					dq.addFirst(new pos(nextR, nextC));
-				} else {
-					dq.addLast(new pos(nextR, nextC));
-				}
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int[][] map = new int[N][M];
+        int[][] dist = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = line.charAt(j) - '0';
             }
         }
-
-        System.out.println(minDist[sizeR - 1][sizeC - 1]);
-    }
-
-    static class pos {
-        int r;
-        int c;
-        pos(int r, int c){
-            this.r = r;
-            this.c = c;
+        for (int[] ints : dist) {
+            Arrays.fill(ints, Integer.MAX_VALUE);
         }
+        Deque<int[]> dq = new ArrayDeque<>();
+        dist[0][0] = 0;
+        dq.add(new int[]{0, 0, 0});
+
+        while (!dq.isEmpty()){
+            int[] curPos = dq.poll();
+
+            for (int d = 0; d < 4; d++) {
+                int nr = curPos[0] + dr[d];
+                int nc = curPos[1] + dc[d];
+
+                if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+
+                if (dist[nr][nc] > curPos[2] + map[nr][nc]){
+                    dist[nr][nc] = curPos[2] + map[nr][nc];
+                    if (map[nr][nc] == 0){
+                        dq.addFirst(new int[] {nr, nc, dist[nr][nc]});
+                    } else {
+                        dq.add(new int[] {nr, nc, dist[nr][nc]});
+                    }
+                }
+            }
+        }
+        System.out.println(dist[N - 1][M - 1]);
     }
 }
