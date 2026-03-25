9085424ea47d0163071f9c7fa80d243b47d5e33f
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-03-25 10:25:11 +0900

    [Gold I] Title: 달빛 여우, Time: 912 ms, Memory: 71880 KB -BaekjoonHub

--- 백준/Gold/16118. 달빛 여우/달빛 여우.java
+++ 백준/Gold/16118. 달빛 여우/달빛 여우.java
@@ -1,115 +1,97 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());   // 나무 그루터기 개수
-        int M = Integer.parseInt(st.nextToken());   // 오솔길 개수
-        
-        List<Edge>[] graph = new ArrayList[N+1];
-        for(int i = 0;i<=N;i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        for(int i = 0;i<M;i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken()) * 2;
-            graph[start].add(new Edge(end, weight));
-            graph[end].add(new Edge(start, weight));
-        }
-
-        long[] minDistRabbit = new long[N+1];
-        Arrays.fill(minDistRabbit, Long.MAX_VALUE);
-        minDistRabbit[1] = 0;
-
-        PriorityQueue<Edge> pqr = new PriorityQueue<>((a, b) -> Long.compare(a.weight, b.weight));
-        pqr.offer(new Edge(1, 0));
-
-        while(!pqr.isEmpty()) {
-            Edge now = pqr.poll();
-
-            if(minDistRabbit[now.end] != now.weight) continue;
-
-            for(Edge e : graph[now.end]) {
-                long nextW = now.weight + e.weight;
-                if(minDistRabbit[e.end] > nextW) {
-                    minDistRabbit[e.end] = nextW;
-                    pqr.offer(new Edge(e.end, nextW));
-                }
-            }
-        }
-
-        long[] oddWolf = new long[N+1];
-        long[] evenWolf = new long[N+1];
-        Arrays.fill(oddWolf, Long.MAX_VALUE);
-        Arrays.fill(evenWolf, Long.MAX_VALUE);
-        // oddWolf[1] = 0;
-        evenWolf[1] = 0;
-
-        PriorityQueue<Edge> pqw = new PriorityQueue<>((a, b) -> Long.compare(a.weight, b.weight));
-        pqw.offer(new Edge(1, 0, 0));
-
-        while(!pqw.isEmpty()) {
-            Edge now = pqw.poll();
-
-            if(now.depth % 2 == 0) {
-                if(evenWolf[now.end] != now.weight) continue;
-            } else {
-                if(oddWolf[now.end] != now.weight) continue;
-            }
-
-            for(Edge e : graph[now.end]) {
-                // 현재 꺼낸 값의 depth를 통해 가중치를 계산한다.
-                // 계산된 가중치를 토대로 다익스트라를 수행한다.
-                long nextW;
-                if(now.depth % 2 == 0) {
-                    // 짝수일때
-                    nextW = now.weight + e.weight / 2;
-                    if(oddWolf[e.end] > nextW) {
-                        oddWolf[e.end] = nextW;
-                        pqw.offer(new Edge(e.end, nextW, now.depth + 1));
-                    }
-                } else {
-                    // 홀수일때
-                    nextW = now.weight + e.weight * 2;
-                    if(evenWolf[e.end] > nextW) {
-                        evenWolf[e.end] = nextW;
-                        pqw.offer(new Edge(e.end, nextW, now.depth + 1));
-                    }
-                }
-            }
-        }
-
-        long[] minDistWolf = new long[N+1];
-
-
-        for(int i = 1;i<=N;i++) {
-            minDistWolf[i] = Math.min(evenWolf[i], oddWolf[i]);
-        }
-
-        int answer = 0;
-        for(int i = 1;i<=N;i++) {
-            if(minDistRabbit[i] < minDistWolf[i]) answer++;
-        }
-        System.out.println(answer);
-    }
-    static class Edge {
-        int end;
-        long weight;
-        int depth;
-        Edge(int end, long weight) {
-            this.end = end;
-            this.weight = weight;
-        }
-        Edge(int end, long weight, int depth) {
-            this.end = end;
-            this.weight = weight;
-            this.depth = depth;
-        }
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		ArrayList<int[]>[] graph = new ArrayList[N + 1];
+		for (int i = 0; i <= N; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int to = Integer.parseInt(st.nextToken());
+			int from = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			graph[to].add(new int[] {from, weight});
+			graph[from].add(new int[] {to, weight});
+		}
+		
+		int[] foxDist = new int[N + 1];
+		Arrays.fill(foxDist, Integer.MAX_VALUE);
+		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+		foxDist[1] = 0;
+		pq.add(new int[] {1, 0});
+		while(!pq.isEmpty()) {
+			int[] curNode = pq.poll();
+			
+			if (foxDist[curNode[0]] < curNode[1]) continue;
+			
+			for (int[] nextNode : graph[curNode[0]]) {
+				int newDist = curNode[1] + nextNode[1];
+				
+				if (foxDist[nextNode[0]] > newDist) {
+					foxDist[nextNode[0]] = newDist;
+					pq.add(new int[] {nextNode[0], newDist});
+				}
+			}
+		}
+		
+		double[][] wolfDist = new double[N + 1][2];				// 0 : 빠름, 1 : 느림
+		for (double[] ds : wolfDist) {
+			Arrays.fill(ds, Double.MAX_VALUE);			
+		}
+		
+		PriorityQueue<double[]> pq2 = new PriorityQueue<>((o1, o2) -> Double.compare(o1[1], o2[1]));
+		wolfDist[1][0] = 0;
+		pq2.add(new double[] {1, 0, 0});
+		
+		while(!pq2.isEmpty()) {
+			double[] curNode = pq2.poll();
+			
+			int node = (int) curNode[0];
+			double dist = curNode[1];
+			int state = (int) curNode[2];						// 0 : 빠름, 1 : 느림
+			
+			if (wolfDist[node][state] < dist) continue;
+			
+			for (int[] nextNode : graph[node]) {
+				double newDist = dist;
+				
+				if (state == 0) {
+					newDist += nextNode[1] / 2.0;
+					if (wolfDist[nextNode[0]][1] > newDist) {
+						wolfDist[nextNode[0]][1] = newDist;
+						pq2.add(new double[]{nextNode[0], newDist, 1});
+					}
+				} else {
+					newDist += nextNode[1] * 2.0;
+					if (wolfDist[nextNode[0]][0] > newDist) {
+						wolfDist[nextNode[0]][0] = newDist;
+						pq2.add(new double[]{nextNode[0], newDist, 0});
+					}
+				}
+			}
+		}
+		
+		int treeStub = 0;
+		
+		for (int i = 2; i <= N; i++) {
+		    if (foxDist[i] == Integer.MAX_VALUE) continue;
+			if (foxDist[i] < wolfDist[i][0] && foxDist[i] < wolfDist[i][1]) treeStub++;
+		}
+		
+		System.out.println(treeStub);
+	}
 }
