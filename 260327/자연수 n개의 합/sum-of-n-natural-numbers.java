41d35c7b0035f61a17b04f76f6b051b41fe16073
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-03-27 17:26:07 +0900

    Title: 자연수 n개의 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260327/자연수 n개의 합/sum-of-n-natural-numbers.java
+++ 260327/자연수 n개의 합/sum-of-n-natural-numbers.java
@@ -3,24 +3,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		long S = Long.parseLong(br.readLine());
-		
-		long left = 1, right = S;
-		long maxNim = 0;
-		
+
+		long left = 1;
+		long right = 2000000000L;
+		long ans = 0;
+
 		while (left <= right) {
 			long mid = (left + right) / 2;
-			
-			if ((mid * (mid + 1)) / 2 <= S) {
-				maxNim = Math.max(maxNim, mid);
+			long sum = mid * (mid + 1) / 2;
+
+			if (sum <= S) {
+				ans = mid;
 				left = mid + 1;
 			} else {
 				right = mid - 1;
 			}
 		}
-		
-		System.out.println(maxNim);
+
+		System.out.println(ans);
 	}
 }
