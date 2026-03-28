4c26637374003dbdc333350e398cbde9b7934401
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-03-28 16:22:47 +0900

    Title: 이차원 배열의 오름차순 정리, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260328/이차원 배열의 오름차순 정리/ascending-order-of-two-dimensional-array.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		long K = Long.parseLong(br.readLine());
+		
+		long left = 1;
+		long right = (long) N * N;
+		long answer = 0;
+		
+		while (left <= right) {
+			long mid = (left + right) / 2;
+			long count = 0;
+			
+			for (int i = 1; i <= N; i++) {
+				count += Math.min((long) N, mid / i);
+			}
+			
+			if (count >= K) {
+				answer = mid;
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		}
+		
+		System.out.println(answer);
+	}
+}
