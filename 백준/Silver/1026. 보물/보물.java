5ebd9c2c1013cfe2f6a3981fdd8373b207885970
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-04-05 23:35:00 +0900

    [Silver IV] Title: 보물, Time: 68 ms, Memory: 11628 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int N;
+    private static int[] A;
+    private static int[] B;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        A = new int[N];
+        B = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += A[i] * B[N - 1 - i];
+        }
+
+        System.out.println(sum);
+    }
+}
