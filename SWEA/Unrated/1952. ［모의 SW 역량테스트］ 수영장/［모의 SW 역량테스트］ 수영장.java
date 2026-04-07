25728435c5f2926516c9f6449d88249581738b2b
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-04-07 14:52:03 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 76 ms, Memory: 25,728 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -4,46 +4,42 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	private static int[] prices = new int[4];					// 1일, 1달, 3달, 1년
-	private static int[] monthPlan = new int[12];
-	private static int minCost;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
 		int T = Integer.parseInt(br.readLine());
-		
+		int[] usePlan = new int[13];
 		for (int tc = 1; tc <= T; tc++) {
-			// 입력 처리
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < 4; i++) {
-				prices[i] = Integer.parseInt(st.nextToken());
-			}
+			// 이용권 가격 : 1일 이용권, 1달 이용권, 3달 이용권, 1년 이용권
 			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < 12; i++) {
-				monthPlan[i] = Integer.parseInt(st.nextToken());
+			int dailyCost = Integer.parseInt(st.nextToken());
+			int monthCost = Integer.parseInt(st.nextToken());
+			int threeMonthCost = Integer.parseInt(st.nextToken());
+			int yearCost = Integer.parseInt(st.nextToken());
+			
+			// 1 ~ 12월 이용 계획
+			st = new StringTokenizer(br.readLine());
+			for (int i = 1; i <= 12; i++) {
+				usePlan[i] = Integer.parseInt(st.nextToken());
 			}
-			minCost = prices[3];								// 최소값을 1년 이용권으로 초기화
-			// 주요 로직 시작
 			
-			dfs(0, 0);
+			int[] monthMin = new int[13];
 			
-			sb.append(String.format("#%d %d\n", tc, minCost));
+			for (int i = 1; i <= 12; i++) {
+				monthMin[i] = Math.min(monthCost, dailyCost * usePlan[i]);
+			}
+			
+			int[] dp = new int[13];
+			dp[1] = monthMin[1];
+			dp[2] = dp[1] + monthMin[2];
+			
+			for (int i = 3; i <= 12; i++) {
+				dp[i] = Math.min(dp[i - 1] + monthMin[i], dp[i - 3] + threeMonthCost);
+			}
+
+			sb.append('#').append(tc).append(' ').append(Math.min(dp[12], yearCost)).append('\n');
 		}
 		System.out.println(sb);
-	}
-	
-	static void dfs(int month, int sum) {
-		if (month >= 12) {
-			minCost = Math.min(minCost, sum);
-			return;
-		}
-		
-		if (sum >= minCost) return;
-		
-		dfs(month + 1, sum + (monthPlan[month] * prices[0]));
-		
-		dfs(month + 1, sum + prices[1]);
-		
-		dfs(month + 3, sum + prices[2]);
 	}
 }
