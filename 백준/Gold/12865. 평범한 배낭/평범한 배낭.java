e3e2d2b19ad261381886cb635e313ebc1930e651
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-04-08 15:23:55 +0900

    [Gold V] Title: 평범한 배낭, Time: 156 ms, Memory: 51456 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		
+		int[] w = new int[N + 1];
+		int[] v = new int[N + 1];
+		
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			w[i] = Integer.parseInt(st.nextToken());
+			v[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int[][] K = new int[N + 1][W + 1];
+		
+		for (int i = 0; i <= N; i++) K[i][0] = 0;
+		for (int i = 0; i <= W; i++) K[0][i] = 0;
+		
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= W; j++) {
+				if (w[i] > j) K[i][j] = K[i - 1][j];
+				
+				else {
+					K[i][j] = Math.max(
+							K[i - 1][j],
+							K[i - 1][j - w[i]] + v[i]
+					);
+				}
+			}
+		}
+
+		System.out.println(K[N][W]);
+	}
+}
