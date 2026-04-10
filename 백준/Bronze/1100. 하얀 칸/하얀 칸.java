29c955e2326a51de60b5d146905ceb6915bd2865
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-04-10 20:45:18 +0900

    [Bronze II] Title: 하얀 칸, Time: 64 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = 0;
+
+        for (int i = 0; i < 8; i++) {
+            String line = br.readLine();
+
+            for (int j = 0; j < 8; j++) {
+                if ((i + j) % 2 == 0 && line.charAt(j) == 'F') {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
