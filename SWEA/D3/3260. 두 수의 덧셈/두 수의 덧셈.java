93cfb6ee0fa00099926f8842a20c60ee92d3a739
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-07-09 09:37:09 +0900

    [D3] Title: 두 수의 덧셈, Time: 97 ms, Memory: 27,776 KB -BaekjoonHub

--- SWEA/D3/3260. 두 수의 덧셈/두 수의 덧셈.java
+++ SWEA/D3/3260. 두 수의 덧셈/두 수의 덧셈.java
@@ -1,52 +1,63 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		for (int testCase = 1; testCase <= T; testCase++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String A = st.nextToken();
-			String B = st.nextToken();
-			String longer = (A.length() >= B.length()) ? A : B;
-			String shorter = (A.length() < B.length()) ? A : B;
-			
-			int longStart = longer.length();
-			int shortStart = shorter.length();
-			Stack<Character> longStack = new Stack<>();
-			Stack<Character> shortStack = new Stack<>();
-			Stack<Integer> result = new Stack<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-			for (int i = 0; i < longStart; i++) {
-				longStack.push(longer.charAt(i));
-			}
-			for (int i = 0; i < shortStart; i++) {
-				shortStack.push(shorter.charAt(i));
-			}
-			while(!shortStack.isEmpty()) {
-				int l = longStack.pop() - '0';
-				int s = shortStack.pop() - '0';
-				result.push(l + s);
-			}
-			while(!longStack.isEmpty()) {
-				result.push(longStack.pop() - '0');
-			}
-			for (int i = 0; i < result.size() - 1; i++) {
-				if (result.get(i) >= 10) {
-					result.set(i, result.get(i) % 10);
-					result.set(i + 1, result.get(i + 1) + 1);;
-				}
-			}
-			sb.append("#" + testCase + " ");
-			while (!result.isEmpty()) {
-				sb.append(result.pop());
-			}
-			sb.append("\n");
-		}
-		System.out.println(sb.toString());
-	}
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            char[] A = st.nextToken().toCharArray();
+            char[] B = st.nextToken().toCharArray();
+
+            Stack<Character> aStack = new Stack<>();
+            Stack<Character> bStack = new Stack<>();
+            Stack<Character> result = new Stack<>();
+
+            for (char c : A) {
+                aStack.push(c);
+            }
+
+            for (char c : B) {
+                bStack.push(c);
+            }
+
+            int carry = 0;
+
+            while (!aStack.isEmpty() || !bStack.isEmpty() || carry > 0) {
+                int a = 0;
+                int b = 0;
+
+                if (!aStack.isEmpty()) {
+                    a = aStack.pop() - '0';
+                }
+
+                if (!bStack.isEmpty()) {
+                    b = bStack.pop() - '0';
+                }
+
+                int sum = a + b + carry;
+
+                result.push((char) (sum % 10 + '0'));
+                carry = sum / 10;
+            }
+
+            sb.append('#').append(i).append(' ');
+
+            while (!result.isEmpty()) {
+                sb.append(result.pop());
+            }
+
+            sb.append('\n');
+        }
+
+        System.out.println(sb);
+    }
 }
