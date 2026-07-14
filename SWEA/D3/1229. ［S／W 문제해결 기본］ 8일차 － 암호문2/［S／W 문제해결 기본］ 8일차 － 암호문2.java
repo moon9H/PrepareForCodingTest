56ea7b6392b4c2b5e6b55c475b23553f32cc09f4
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-07-14 14:35:56 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문2, Time: 77 ms, Memory: 25,472 KB -BaekjoonHub

--- SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.java
+++ SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.java
@@ -1,46 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) throws IOException{
-		int T = 10;
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		for (int testCase = 1; testCase <= T; testCase++) {
-			int N = Integer.parseInt(br.readLine());
-			LinkedList<Integer> list = new LinkedList<>();
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < N; i++) {
-				list.add(Integer.parseInt(st.nextToken()));
-			}
-			int commandNum = Integer.parseInt(br.readLine());
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < commandNum; i++) {
-				String cmd = st.nextToken();
-				if (cmd.equals("I")) {
-					int insertPos = Integer.parseInt(st.nextToken());
-					int insertCnt = Integer.parseInt(st.nextToken());
-					for (int j = 0; j < insertCnt; j++) {
-						list.add(insertPos + j, Integer.parseInt(st.nextToken()));
-					}
-				} else if (cmd.equals("D")) {
-					int deletePos = Integer.parseInt(st.nextToken());
-					int deleteNum = Integer.parseInt(st.nextToken());
-					for (int j = 0; j < deleteNum; j++) {
-						list.remove(deletePos);
-					}
-				}
-			}
-			sb.append("#").append(testCase);
-			for (int i = 0; i < 10; i++) {
-				sb.append(" ").append(list.get(i));
-			}
-			sb.append("\n");
-		}
-		System.out.println(sb.toString());
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = null;
+        int T = 10;
+        for (int tc = 1; tc <= 10; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            List<Integer> crypto = new ArrayList<>();
+
+            st = new StringTokenizer(br.readLine());
+
+            while(st.hasMoreTokens()) crypto.add(Integer.parseInt(st.nextToken()));
+
+            int cmdLen = Integer.parseInt(br.readLine());
+
+            st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < cmdLen; i++) {
+                // 삽입 명령 구현
+               if (st.nextToken().equals("I")) {
+                    int x = Integer.parseInt(st.nextToken());
+                    int y = Integer.parseInt(st.nextToken());
+                   for (int j = 0; j < y; j++) {
+                       crypto.add(x + j, Integer.parseInt(st.nextToken()));
+                   }
+               }
+
+               // 삭제 명령 구현
+               else {
+                    int x = Integer.parseInt(st.nextToken());
+                    int y = Integer.parseInt(st.nextToken());
+
+                   for (int j = 0; j < y; j++) {
+                       crypto.remove(x);
+                   }
+               }
+            }
+
+            sb.append('#').append(tc).append(' ');
+            for (int i = 0; i < 10; i++) {
+                sb.append(crypto.get(i)).append(' ');
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
 }
