0f8f755b27238c3f3b824ba180ece34018281dca
Moongyu Hwang <127906176+moon9H@users.noreply.github.com>
2026-07-14 09:21:07 +0900

    [D4] Title: 쇠막대기 자르기, Time: 158 ms, Memory: 39,348 KB -BaekjoonHub

--- SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.java
+++ SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.java
@@ -4,33 +4,33 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Solution {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		for (int testCase = 1; testCase <= T; testCase++) {
-			String pipeline = br.readLine();
-			Stack<Character> stack = new Stack<>();
-			int result = 0;
-			for (int i = 0; i < pipeline.length(); i++) {
-				if (pipeline.charAt(i) == '(') {
-					stack.push('(');
-				} else {
-					if (pipeline.charAt(i - 1) == '(') {
-						stack.pop();
-						if (!stack.isEmpty()) {
-							result += stack.size();
-						}
-					} else {
-						stack.pop();
-						result++;
-					}
-					
-				}
-			}
-			sb.append("#" + testCase + " ").append(result).append("\n");
-		}
-		System.out.println(sb.toString());
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++) {
+            char[] lazerPipe = br.readLine().toCharArray();
+            Stack<Character> stack = new Stack<>();
+            int pipeUnderLazerCnt = 0;
+
+            for (int i = 0; i < lazerPipe.length; i++) {
+                if (lazerPipe[i] == '(') stack.push('(');
+                else {
+                    if (lazerPipe[i - 1] == '('){
+                        stack.pop();
+                        pipeUnderLazerCnt += stack.size();
+                    }
+                    else {
+                        stack.pop();
+                        pipeUnderLazerCnt += 1;
+                    }
+                }
+            }
+
+            sb.append('#').append(tc).append(' ').append(pipeUnderLazerCnt).append('\n');
+        }
+        System.out.println(sb);
+    }
 }
